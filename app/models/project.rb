d140f242c87acf2f8bcf23eb964de8070bf5395e
Johan Sørensen <johan@johansorensen.com>
2007-11-19 20:58:55 +0100

    some helper Project associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project < ActiveRecord::Base
-  belongs_to :user
-  has_many :repositories
+  belongs_to  :user
+  has_many    :repositories
+  has_one     :mainline_repository, :conditions => ["mainline = ?", true], 
+    :class_name => "Repository"
+  has_many    :branch_repositories, :conditions => ["mainline = ?", false],
+    :class_name => "Repository"
   
   validates_presence_of :title, :user_id, :slug
   validates_format_of :slug, :with => /^[a-z0-9_\-]+$/, 
