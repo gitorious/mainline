238f6e42184a72715912b5e1a6e74a22a2a6370c
Johan Sørensen <johan@johansorensen.com>
2007-11-29 23:55:36 +0100

    ssh key linting and display methods

--- app/models/ssh_key.rb
+++ app/models/ssh_key.rb
@@ -3,4 +3,16 @@ class SshKey < ActiveRecord::Base
   
   validates_presence_of :user_id, :key
   validates_format_of   :key, :with => /^ssh-[a-z0-9]{3,4} .+$/ims
+  
+  before_save :lint_key!
+  
+  def display_key(cols=72)
+    key.gsub(/(.{1,#{cols}})/, "\\1\n").strip
+  end
+  
+  
+  protected
+    def lint_key!
+      key.gsub!(/\n+/ms, "")
+    end
 end
--- spec/models/ssh_key_spec.rb
+++ spec/models/ssh_key_spec.rb
@@ -5,8 +5,8 @@ describe SshKey do
   def create_key(opts={})
     SshKey.new({
       :user_id => 1,
-      :key => "ssh-rsa bXljYWtkZHlpemltd21vY2NqdGJnaHN2bXFjdG9zbXplaGlpZnZ0a3VyZWFz\nc2dkanB4aXNxamxieGVib3l6Z3hmb2ZxZW15Y2FrZGR5aXppbXdtb2NjanRi\nZ2hzdm1xY3Rvc216ZWhpaWZ2dGt1cmVhc3NnZGpweGlzcWpsYnhlYm95emd4\nZm9mcWU= foo@example.com",
-    })
+      :key => "ssh-rsa bXljYWtkZHlpemltd21vY2NqdGJnaHN2bXFjdG9zbXplaGlpZnZ0a3VyZWFzc2dkanB4aXNxamxieGVib3l6Z3hmb2ZxZW15Y2FrZGR5aXppbXdtb2NjanRiZ2hzdm1xY3Rvc216ZWhpaWZ2dGt1cmVhc3NnZGpweGlzcWpsYnhlYm95emd4Zm9mcWU= foo@example.com",
+    }.merge(opts))
   end
 
   it "should have a valid ssh key" do
@@ -28,5 +28,16 @@ describe SshKey do
     key.user_id = users(:johan).id
     key.valid?
     key.should be_valid
+  end
+  
+  it "strips newlines before save" do
+    ssh = create_key(:key => "ssh-rsa bXljYWtkZHlpemltd21vY2NqdGJnaHN2bXFjdG\n9zbXplaGlpZnZ0a3VyZWFzc2dkanB4aXNxamxieGVib3l6Z3hmb2ZxZW15Y2FrZGR5aXppbXdtb2NjanRiZ2hzdm1xY3Rvc216ZWhpaWZ2dGt1cm\nVhc3NnZGpweGlzcWpsYnhlYm95emd4Zm9mcWU= foo@example.com")
+    ssh.save
+    ssh.key.should_not include("\n")
+  end
+    
+  it "wraps the key at 72 for display" do
+    ssh = create_key
+    ssh.display_key.should include("\n")
   end
 end
