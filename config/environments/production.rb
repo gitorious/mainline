1a4c3b81c062e6ce323033f6d54c77940cd14340
Johan Sørensen <johan@johansorensen.com>
2008-01-06 00:56:07 +0100

    Hodel3000 compability

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,9 @@ config.cache_classes = true
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 
+require 'hodel_3000_compliant_logger'
+config.logger = Hodel3000CompliantLogger.new(config.log_path)
+
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
