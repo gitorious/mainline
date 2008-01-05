676d46ea09324d5c79d33bc6947004accf59385a
Johan Sørensen <johan@johansorensen.com>
2008-01-05 02:31:39 +0100

    update user_mailer urls

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://gitorious.org/users/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://gitorious.org/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "no-reply@gitorious.org"
+      @subject     = "[Gitorious] "
       @sent_on     = Time.now
       @body[:user] = user
     end
