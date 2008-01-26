ca42b0384febd656bcdc458d0dfac95c060eae41
Johan Sørensen <johan@johansorensen.com>
2008-01-26 15:57:37 +0100

    list recent projects on homepage

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,7 @@ class SiteController < ApplicationController
   
   def index
     @tags = Project.tag_counts
+    @projects = Project.find(:all, :limit => 5, :order => "id desc")
   end
   
   def about
--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -2,9 +2,22 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe SiteController do
 
-  #Delete this example and add some real ones
-  it "should use SiteController" do
-    controller.should be_an_instance_of(SiteController)
+  describe "#index" do
+    
+    it "GETs sucessfully" do
+      get :index
+      response.should be_success
+    end
+    
+    it "gets the tag list" do
+      get :index
+      assigns[:tags].should == Project.tag_counts
+    end
+    
+    it "gets a list of the most recent projects" do
+      get :index
+      assigns[:projects].should == Project.find(:all, :limit => 5, :order => "id desc")
+    end
   end
 
 end
