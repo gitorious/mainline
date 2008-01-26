e71fc912d2c854306001a32aa5cc3e58847c8c8e
Johan Sørensen <johan@johansorensen.com>
2008-01-26 23:40:43 +0100

    sort the cloned repos list on the dashboard

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -12,9 +12,8 @@ class SiteController < ApplicationController
     @recent_comments = Comment.find(:all, :limit => 10,
       :conditions => ["comments.project_id in (?)", project_ids], 
       :order => "comments.created_at desc", :include => [:user, :repository])
-    @repository_clones = @projects.map(&:repository_clones).flatten
-    # @repository_clones = Repository.find(:all, 
-    #   :conditions => ["project_id in (?) and mainline = ?", project_ids, false])
+    @repository_clones = Repository.find(:all, :order => "created_at desc",
+      :conditions => ["project_id in (?) and mainline = ?", project_ids, false])
   end
   
   def about
