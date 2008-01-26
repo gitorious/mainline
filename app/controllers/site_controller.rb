ec260430b924fd7f8fcb9499aa8aba70569e0363
Johan Sørensen <johan@johansorensen.com>
2008-01-26 22:43:18 +0100

    less 1+N queries on dashboard

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -10,7 +10,8 @@ class SiteController < ApplicationController
     @projects = current_user.projects
     project_ids = @projects.map(&:id)
     @recent_comments = Comment.find(:all, :limit => 10,
-      :conditions => ["project_id in (?)", project_ids], :order => "created_at desc")
+      :conditions => ["comments.project_id in (?)", project_ids], 
+      :order => "comments.created_at desc", :include => [:user, :repository])
     @repository_clones = @projects.map(&:repository_clones).flatten
     # @repository_clones = Repository.find(:all, 
     #   :conditions => ["project_id in (?) and mainline = ?", project_ids, false])
