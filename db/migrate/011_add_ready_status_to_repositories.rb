d7916914c1f2b76c2c988fbc5aa45584f97c3025
David A. Cuadrado <krawek@gmail.com>
2008-02-25 23:32:39 -0500

    FIXED: use false instead of 0

--- db/migrate/011_add_ready_status_to_repositories.rb
+++ db/migrate/011_add_ready_status_to_repositories.rb
@@ -1,6 +1,6 @@
 class AddReadyStatusToRepositories < ActiveRecord::Migration
   def self.up
-    add_column :repositories, :ready, :boolean, :default => 0
+    add_column :repositories, :ready, :boolean, :default => false
     add_index :repositories, :ready
   end
 
