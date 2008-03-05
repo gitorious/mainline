8f22308c80b867072067ff3107c756b01e375441
David Chelimsky <dchelimsky@gmail.com>
2008-03-05 16:15:55 -0600

    Removed unused helper spec files (left one example pending that should be added).

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,11 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe UsersHelper do
-  
-  #Delete this example and add some real ones or delete this file
-  it "should include the UsersHelper" do
-    included_modules = self.metaclass.send :included_modules
-    included_modules.should include(UsersHelper)
-  end
-  
+  it "should encode email"  
 end
