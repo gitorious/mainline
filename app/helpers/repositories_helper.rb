fa2d6392e179bf4ab9b320572a916c0479673f32
David A. Cuadrado <krawek@gmail.com>
2008-03-06 19:53:49 -0500

    Link users to commits if email match

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,2 +1,13 @@
 module RepositoriesHelper
+  # Returns a Hash {email => user}
+  def self.users_by_commits(commits)
+    emails = commits.map { |commit| commit.committer.email }
+    users = User.find(:all, :conditions => ["email in (?)", emails])
+    email_user = {}
+    users.each { |user|
+      email_user[user.email] = user
+    }
+    
+    email_user
+  end
 end
