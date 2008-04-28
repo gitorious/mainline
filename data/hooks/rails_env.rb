35ad4575858b22e34b6a4426e6d933d7b0a0c9a9
Johan Sørensen <johan@johansorensen.com>
2008-04-28 21:11:49 +0200

    Print less from *-receive hooks

--- data/hooks/rails_env.rb
+++ data/hooks/rails_env.rb
@@ -2,9 +2,8 @@
 current_dir = File.expand_path(File.dirname(__FILE__))
 app_root = File.join(current_dir, "../../")
 
-print "=> Wait... "
+print "=> Syncing Gitorious... "
 $stdout.flush
 ENV["RAILS_ENV"] ||= "production"
 require File.join(app_root,"/config/environment")
 
-puts "[OK]"
