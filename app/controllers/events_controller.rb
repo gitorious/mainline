09fa09373a192d3659dc58b4ad97d26061fd8b82
David A. Cuadrado <krawek@gmail.com>
2008-04-29 16:59:03 -0500

    FIXED: order events desc
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.paginate(:all, :order => "events.created_at asc", 
+    @events = Event.paginate(:all, :order => "events.created_at desc", 
                   :page => params[:page], :include => [:user])
     @atom_auto_discovery_url = formatted_events_path(:atom)
     
