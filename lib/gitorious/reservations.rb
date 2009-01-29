258611d9b805bddacbf097839efdedda9bce15ae
Johan Sørensen <johan@johansorensen.com>
2009-01-29 16:02:33 +0100

    Silence const_defined warnings

--- lib/gitorious/reservations.rb
+++ lib/gitorious/reservations.rb
@@ -1,12 +1,14 @@
 module Gitorious
   module Reservations
-    UNACCOUNTED_ROOT_NAMES = ["teams", "dashboard", "about", "login", "logout"] 
-    RESERVED_ROOT_NAMES = UNACCOUNTED_ROOT_NAMES + Dir[File.join(RAILS_ROOT, "public", "*")].map{|f| File.basename(f) }
-    CONTROLLER_NAMES_PLURAL = ActionController::Routing.possible_controllers.map{|s| s.split("/").first }
-    CONTROLLER_NAMES = CONTROLLER_NAMES_PLURAL + CONTROLLER_NAMES_PLURAL.map{|s| s.singularize }
-    PROJECTS_MEMBER_ACTIONS = %w[edit update destroy confirm_delete]
+    silence_warnings do
+      UNACCOUNTED_ROOT_NAMES = ["teams", "dashboard", "about", "login", "logout"]
+      RESERVED_ROOT_NAMES = UNACCOUNTED_ROOT_NAMES + Dir[File.join(RAILS_ROOT, "public", "*")].map{|f| File.basename(f) }
+      CONTROLLER_NAMES_PLURAL = ActionController::Routing.possible_controllers.map{|s| s.split("/").first }
+      CONTROLLER_NAMES = CONTROLLER_NAMES_PLURAL + CONTROLLER_NAMES_PLURAL.map{|s| s.singularize }
+      PROJECTS_MEMBER_ACTIONS = %w[edit update destroy confirm_delete]
     
-    PROJECT_NAMES = RESERVED_ROOT_NAMES + CONTROLLER_NAMES
-    REPOSITORY_NAMES = PROJECTS_MEMBER_ACTIONS + CONTROLLER_NAMES
+      PROJECT_NAMES = RESERVED_ROOT_NAMES + CONTROLLER_NAMES
+      REPOSITORY_NAMES = PROJECTS_MEMBER_ACTIONS + CONTROLLER_NAMES
+    end
   end
 end
