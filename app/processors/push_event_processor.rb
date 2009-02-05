5603e0e97adb379bd623e712727a2f9aa8a7a3a4
Marius Mathiesen <marius.mathiesen@gmail.com>
2009-02-05 16:21:41 +0100

    Refactoring the push event processor with spec coverage

--- app/processors/push_event_processor.rb
+++ app/processors/push_event_processor.rb
@@ -1,85 +1,123 @@
 class PushEventProcessor < ApplicationProcessor
 
   subscribes_to :push_event
-  attr_reader :oldrev, :newrev, :revname
+  attr_reader :oldrev, :newrev, :action
   
   def on_message(message)
     hash = ActiveSupport::JSON.decode(message)
     logger.debug("Processor on message #{hash.inspect}")
     if @repository = Repository.find_by_hashed_path(hash['gitdir'])
       self.commit_summary = hash['message']
-      event = extract_event(@repository, hash['message'])
-      logger.debug("Processor found repository #{@repository.id}, email: #{committer_email}")
+      log_events
     else
-      logger.debug("Processor received message, but couldn't find repo with hashed_path #{hash['gitdir']}")
+      logger.error("Processor received message, but couldn't find repo with hashed_path #{hash['gitdir']}")
     end
+  end
+  
+  def log_events
+    events.each do |e|
+      log_event(e)
+    end
+  end
+  
+  def log_event(an_event)
+    @project ||= Project.find_by_slug("bar")
+    logger.debug("Processor: adding event #{an_event.message} from #{an_event.email}")
+    @project.create_event(an_event.event_type, @repository, User.find_by_email(an_event.email), an_event.identifier, an_event.message, an_event.commit_time)
+  end
+  
+  # Sets the commit summary, as served from git
+  def commit_summary=(spec)
+    @oldrev, @newrev, revname = spec.split(' ')
+    r, name, @identifier = revname.split("/")
+    @head_or_tag = name == 'tags' ? :tag : :head
+  end
+  
+  def head?
+    @head_or_tag == :head
+  end
+  
+  def tag?
+    @head_or_tag == :tag
+  end
+  
+  def action
+    @action ||= if oldrev =~ /^0+$/
+      :create
+    elsif newrev =~ /^0+$/
+      :delete
+    else
+      :update
+    end
+  end
+  
+  def events
+    @events ||= fetch_events
+  end
+  
+  class EventForLogging
+    attr_accessor :event_type, :identifier, :email, :message, :commit_time
+    def to_s
+      "Type: #{event_type} by #{email} at #{commit_time} with #{identifier}"
+    end
+  end
+  
+  def fetch_events
+    if tag?
+      e = EventForLogging.new
+      e.event_type = action == :create ? Action::CREATE_TAG : Action::DELETE_TAG
+      e.identifier = @identifier
+      fetch_commit_details(e, @newrev)
+      return [e]
+    elsif action == :create
+      e = EventForLogging.new
+      e.event_type = Action::CREATE_BRANCH
+      e.identifier = @identifier
+      fetch_commit_details(e, @newrev)
+      return [e]
+    elsif action == :delete
+      e = EventForLogging.new
+      e.event_type = Action::DELETE_BRANCH
+      e.identifier = @identifier
+      fetch_commit_details(e, @oldrev)
+      return [e]
+    else
+      events_from_git_log
+    end
+  end
+  
+  GIT_SEPARATOR = ";;"
+  
+  def fetch_commit_details(an_event, commit_sha)
+    sha, email, timestamp, message = git.show({:pretty => git_pretty_format, :s => true}, commit_sha).split(GIT_SEPARATOR)
+    an_event.email        = email
+    an_event.commit_time  = Time.at(timestamp.to_i).utc
+    an_event.message      = message
+    logger.info("Processor returning #{an_event}")
+  end
+  
+  def events_from_git_log
+    result = []
+    commits = git.log({:pretty => git_pretty_format, :s => true}, "#{@oldrev}..#{@newrev}").split("\n")
+    commits.each do |c|
+      sha, email, timestamp, message = c.split(GIT_SEPARATOR)
+      e = EventForLogging.new
+      e.identifier    = sha
+      e.email         = email
+      e.commit_time   = Time.at(timestamp.to_i).utc
+      e.event_type    = Action::COMMIT
+      e.message       = message
+      result << e
+    end
+    return result
   end
   
   def git
     @git ||= @repository.git.git
   end
   
-  def commit_summary=(a_line)
-    @oldrev, @newrev, @revname = a_line.split(' ')
-  end
-  
-  def revision
-    @revision ||= case event_type
-    when Action::DELETE_BRANCH
-      @oldrev
-    else
-      @newrev
-    end
-  end
-
-  def committer_email
-    result = [git.show({:pretty => 'format:%ce', :s => true}, revision)]
-    return result
-  end
-  
-  def emails_and_messages
-    git_spec = case event_type
-    when Action::COMMIT
-      "#{@oldrev}..#{@newrev}"
-    else
-      @newrev
-    end
-#    revs = git.rev_list({}, git_spec).split("\n")
-    addresses, messages, dates, revs = git.log({:pretty => 'format:%ce;%s;%ct;%H'}, git_spec).split("\n").collect{|line|line.split(';')}
-    return [addresses, messages, dates, revs]
-  end
-  
-  def committer
-    logger.debug("Processor looking for user with email: '#{committer_email}'")
-    logger.debug("Processor processing commits #{emails_and_messages}")
-    User.find_by_email(committer_email)
-  end
-  
-  def event_type
-    if oldrev =~ /^0+$/
-      action = Action::CREATE_BRANCH
-    else
-      if newrev =~ /^0+$/
-        action = Action::DELETE_BRANCH
-      else
-        action = Action::COMMIT
-      end
-    end
-    return action
-  end
-  
-  
-  protected
-  def extract_event(a_repo, message)
-    project = Project.find_by_slug('bar')
-    begin 
-      emails_and_messages.each do |address, message, date, rev|
-        logger.debug("Processor got #{address}, #{message}, #{date}, #{rev}")
-        project.create_event(event_type, a_repo, User.find_by_email(address), rev, message, Time.at(date.to_i).utc)
-      end
-      # project.create_event(event_type, a_repo, committer, revision, 'Unknown', Time.now)
-    rescue
-      logger.error("Processor got error #{$!}")
-    end
+  def git_pretty_format
+    fmt = ['%H','%ce','%at','%s'].join(GIT_SEPARATOR)
+    "format:#{fmt}"
   end
 end
--- spec/processors/push_event_processor_spec.rb
+++ spec/processors/push_event_processor_spec.rb
@@ -23,12 +23,92 @@ describe PushEventProcessor do
     @processor = PushEventProcessor.new    
   end
   
-  it 'classifies a commit by type' do
-    @processor.commit_summary = "#{'0'*40} #{'a'*40} refs/heads/master"
-    @processor.event_type.should == Action::CREATE_BRANCH
-    @processor.commit_summary = "#{'b'*40} #{'0'*40} refs/heads/master"
-    @processor.event_type.should == Action::DELETE_BRANCH
-    @processor.commit_summary = "#{'a'*40} #{'b'*40} refs/heads/master"
-    @processor.event_type.should == Action::COMMIT
+  it 'returns the correct type and identifier for a new tag' do
+    stub_git_show
+    @processor.commit_summary = "0000000000000000000000000000000000000000 a9934c1d3a56edfa8f45e5f157869874c8dc2c34 refs/tags/r1.1"
+    @processor.action.should == :create
+    @processor.should be_tag
+    @processor.events.size.should == 1
+    @processor.events.first.event_type.should == Action::CREATE_TAG
+    @processor.events.first.identifier.should == 'r1.1'
+    @processor.expects(:log_event).once
+    @processor.log_events
   end
+  
+  it 'returns the correct type and identifier for a new branch' do
+    stub_git_show
+    @processor.commit_summary = '0000000000000000000000000000000000000000 a9934c1d3a56edfa8f45e5f157869874c8dc2c34 refs/heads/foo_branch'
+    @processor.action.should == :create
+    @processor.should be_head
+    @processor.events.size.should == 1
+    @processor.events.first.event_type.should == Action::CREATE_BRANCH
+    @processor.events.first.identifier.should == 'foo_branch'
+    @processor.expects(:log_event).once
+    @processor.log_events    
+  end
+  
+  it 'returns the correct type and a set of events for a commit' do
+    git = mock
+    output = ['33f746e21ef5122511a5a69f381bfdf017f4d66c', 'john@nowhere.com','1233842115','This is really nice'].join(PushEventProcessor::GIT_SEPARATOR) + "\n"
+    git.stubs(:log).returns(output*3)
+    @processor.stubs(:git).returns(git)
+    @processor.commit_summary = "a9934c1d3a56edfa8f45e5f157869874c8dc2c34 33f746e21ef5122511a5a69f381bfdf017f4d66c refs/heads/foo_branch"
+    @processor.action.should == :update
+    @processor.should be_head
+    @processor.events.size.should == 3
+    first_event = @processor.events.first
+    first_event.event_type.should == Action::COMMIT
+    first_event.identifier.should == '33f746e21ef5122511a5a69f381bfdf017f4d66c'
+    first_event.email.should == 'john@nowhere.com'
+    @processor.expects(:log_event).times(3)
+    @processor.log_events
+  end
+  
+  it 'returns the correct type and identifier for the deletion of a tag' do
+    stub_git_show
+    @processor.commit_summary = "a9934c1d3a56edfa8f45e5f157869874c8dc2c34 0000000000000000000000000000000000000000 refs/tags/r1.1"
+    @processor.action.should == :delete
+    @processor.should be_tag
+    @processor.events.size.should == 1
+    @processor.events.first.event_type.should == Action::DELETE_TAG
+    @processor.events.first.identifier.should == 'r1.1'
+    @processor.events.first.email.should == 'john@nowhere.com'
+    @processor.events.first.message.should == 'Whoops, deleting the tag'
+    @processor.expects(:log_event).once
+    @processor.log_events
+  end
+  
+  it 'returns the correct type and identifier for the deletion of a branch' do
+    stub_git_show
+    @processor.commit_summary = 'a9934c1d3a56edfa8f45e5f157869874c8dc2c34 0000000000000000000000000000000000000000 refs/heads/foo_branch'
+    @processor.action.should == :delete
+    @processor.should be_head
+    @processor.events.size.should == 1
+    @processor.events.first.event_type.should == Action::DELETE_BRANCH
+    @processor.events.first.identifier.should == 'foo_branch'
+    @processor.expects(:log_event).once
+    @processor.log_events
+  end
+  
+  def stub_git_show
+    git = mock
+    output = ["a9934c1d3a56edfa8f45e5f157869874c8dc2c34","john@nowhere.com","1233842115","Whoops, deleting the tag"].join(PushEventProcessor::GIT_SEPARATOR)
+    git.stubs(:show).returns(output)
+    @processor.stubs(:git).returns(git)    
+  end
+  
+  # describe 'with stubbing towards a live repo' do
+  #   before(:each) do
+  #     @repo = Grit::Repo.new("/Users/marius/tmp/clone")
+  #     @processor.stubs(:git).returns(@repo.git)
+  #   end
+  #   
+  #   it 'should get decent output from git log' do
+  #     @processor.commit_summary = "b808ca5eb8ab40a9fdc3489f9f83f6cf6e726a61 abf17983b01f716f34ee10b3f74f14fe7f3bf4ed refs/heads/master"
+  #     @processor.events.size.should == 1
+  #     @processor.events.first.email.should == 'marius.mathiesen@gmail.com'
+  #     @processor.events.first.message.should == 'Adding some stuff'
+  #   end
+  # end
 end
+
