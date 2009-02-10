1329f9137bd8eb2f10c458e4ab086b5d3279d847
Johan Sørensen <johan@johansorensen.com>
2009-02-10 10:36:45 +0100

    Default to have the committer Role first in listings/selects/etc

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,13 @@ class Role < ActiveRecord::Base
   KIND_ADMIN = 0
   KIND_COMMITTER = 1
   
+  # TODO: use this when we upgrade to rails 2.3, and nuke the ::all override
+  # default_scope :order => 'kind desc'
+  
+  def self.all
+    find(:all, :order => 'kind desc')
+  end
+  
   def self.admin
     find_by_kind(KIND_ADMIN)
   end
