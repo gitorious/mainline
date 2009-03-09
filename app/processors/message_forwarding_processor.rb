95b1366e46e401d76bc7ca8a0e40612ffad6beb1
Marius Mathiesen <marius.mathiesen@gmail.com>
2009-03-09 13:21:03 +0100

    Adding notifiable support to messages:
    - Link a merge request with a message
    - Display link in message (web)

--- app/processors/message_forwarding_processor.rb
+++ app/processors/message_forwarding_processor.rb
@@ -8,10 +8,15 @@ class MessageForwardingProcessor < ApplicationProcessor
     sender_id = message_hash['sender_id']
     subject = message_hash['subject']
     body = message_hash['body']
+    notifiable_type = message_hash['notifiable_type']
+    notifiable_id = message_hash['notifiable_id']
     begin
       recipient = User.find(recipient_id)
       sender = User.find(sender_id)
-      Mailer.deliver_notification_copy(recipient, sender, subject, body)
+      notifiable = if !notifiable_type.blank?
+        notifiable_type.constantize.find(notifiable_id)
+      end
+      Mailer.deliver_notification_copy(recipient, sender, subject, body, notifiable)
     rescue ActiveRecord::RecordNotFound
       logger.error("Could not deliver message to #{recipient_id}")
     end
