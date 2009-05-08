ded8474ea7a62638d2914cfb9b730a3515329b12
Marius Mathiesen <marius.mathiesen@gmail.com>
2009-05-08 14:45:10 +0200

    The post receive hook now supports overriding the hostname/ip and port for the Stomp MQ server: - stomp_server_address is the hostname of the server to receive messages - stomp_server_port is the port number
    RAILS_ENV=production is assumed, and defaults are provided ("localhost" and 61613, respectively)

--- data/hooks/messaging.rb
+++ data/hooks/messaging.rb
@@ -18,14 +18,24 @@
 require 'rubygems'
 require 'stomp'
 require 'json'
+require 'yaml'
 
 print "=> Syncing Gitorious... "
 
+RAILS_ENV = 'production'
 
 class Publisher
   def connect
-    @connection = Stomp::Connection.open(nil, nil, 'localhost', 61613, true)
+    stomp_server, stomp_port = stomp_server_and_port
+    @connection = Stomp::Connection.open(nil, nil, stomp_server, stomp_port, true)
     @connected = true
+  end
+  
+  def stomp_server_and_port
+    gitorious_yaml = YAML::load_file(File.join(File.dirname(__FILE__), "..", "..", "config", "gitorious.yml"))[RAILS_ENV]
+    server = gitorious_yaml['stomp_server_address'] || 'localhost'
+    host = (gitorious_yaml['stomp_server_port'] || '61613').to_i
+    return [server, host]
   end
 
   def post_message(message)
