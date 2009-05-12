gitorious/mainline

commit 4e4c37084aa60f34fe4e7fe578ea4974a2609b6d

Make sure the back link on confirm_delete links to the right owner, and that it requires adminship

app/views/repositories/confirm_delete.html.erb
@@ -32,6 +32,7 @@
 
 <% content_for :sidebar do -%>
   <ul class="links">
-    <li><%= link_to t("views.projects.back"), edit_project_repository_path(@owner, @repository) -%></li>
+    <li><%= link_to t("views.projects.back"), 
+	repo_owner_path(@repository, :edit_project_repository_path, @owner, @repository) -%></li>
   </ul>
 <% end -%>

app/controllers/repositories_controller.rb
@@ -174,6 +174,10 @@ class RepositoriesController < ApplicationController
   
   def confirm_delete
     @repository = @owner.repositories.find_by_name_in_project!(params[:id], @containing_project)
+    unless @repository.can_be_deleted_by?(current_user)
+      flash[:error] = I18n.t("repositories_controller.only_projects_create_new_error")
+      redirect_to(@owner) and return
+    end
   end
   
   def destroy

commit 30bee890c3ea4bc7bea8f39936b79e7cfcb86e27

Link back to the proper url from confirm_delete
Regression introduced in 4e4c37084

app/views/repositories/confirm_delete.html.erb
@@ -33,6 +33,6 @@
 <% content_for :sidebar do -%>
   <ul class="links">
     <li><%= link_to t("views.projects.back"), 
-	repo_owner_path(@repository, :edit_project_repository_path, @owner, @repository) -%></li>
+	repo_owner_path(@repository, [:edit, @repository.project, @repository]) -%></li>
   </ul>
 <% end -%>

test/functional/repositories_controller_test.rb
@@ -696,6 +696,17 @@ class RepositoriesControllerTest < ActionController::TestCase
         assert_response :redirect
       end
     end
+    
+    should "work for user/group clones" do
+      repo = repositories(:johans2)
+      repo.user = users(:mike)
+      repo.save!
+      login_as :mike
+      get :confirm_delete, :group_id => repo.owner.to_param, 
+        :project_id => repo.project.to_param, :id => repo.to_param
+      assert_response :success
+      assert_template "confirm_delete"
+    end
   end
 
   context "new / create" do