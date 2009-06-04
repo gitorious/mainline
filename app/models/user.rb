0c7597b1f295601c85784cee50e2ba6e1793ec14
Johan Sørensen <johan@johansorensen.com>
2009-06-04 10:26:46 +0200

    Make sure a users email_aliases and committerships have a destroy dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,13 +31,13 @@ class User < ActiveRecord::Base
   has_many :groups, :through => :memberships
   has_many :repositories, :as => :owner, :conditions => ["kind != ?", Repository::KIND_WIKI],
     :dependent => :destroy
-  has_many :committerships, :as => :committer
+  has_many :committerships, :as => :committer, :dependent => :destroy
   has_many :commit_repositories, :through => :committerships, :source => :repository,
     :conditions => ["repositories.kind != ?", Repository::KIND_WIKI]
   has_many :ssh_keys, :order => "id desc"
   has_many :comments
   has_many :events, :order => "events.created_at asc", :dependent => :destroy
-  has_many :email_aliases, :class_name => "Email"
+  has_many :email_aliases, :class_name => "Email", :dependent => :destroy
   
   # Virtual attribute for the unencrypted password
   attr_accessor :password, :current_password
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -33,7 +33,9 @@ class UserTest < ActiveSupport::TestCase
   end
   
   should_have_many :email_aliases
-  should_have_many :committerships
+  should_have_many :committerships, :dependent => :destroy
+  should_have_many :memberships, :dependent => :destroy
+  should_have_many :email_aliases, :dependent => :destroy
   should_have_many :commit_repositories
   should_validate_presence_of :login, :password, :password_confirmation, :email
   should_validate_acceptance_of :terms_of_use
