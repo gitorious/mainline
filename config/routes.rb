b348e5d2251d24e2e8450fea474faff015f31e5f
Johan Sørensen <johan@johansorensen.com>
2009-06-08 13:12:13 +0200

    Add copyright headers and a note about the repository_owner_namespacing filter to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies)
+#   Copyright (C) 2007, 2008, 2009 Johan Sørensen <johan@johansorensen.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
+
+# == ROUTING NOTE =========================================================
+#
+# Note that all routes are getting pre and post processed by the filter class
+# in RAILS_ROOT/lib/route_filters/repository_owner_namespacing.rb and that 
+# you should be EXTRA CAREFUL when adding a route that doesn't map directly
+# by name to an existing controller or action. In such case the same string
+# should be added to Gitorious::Reservations::unaccounted_root_names
+#
+# =========================================================================
+
 ActionController::Routing::Routes.draw do |map|
   VALID_REF = /[a-zA-Z0-9~\{\}\+\^\.\-_\/]+/
   
