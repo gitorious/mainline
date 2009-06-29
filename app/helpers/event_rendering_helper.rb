f17bf1d1a1133b80b0a0917ed71d14fe0ba8d3ad
Johan Sørensen <johan@johansorensen.com>
2009-06-29 14:50:49 +0200

    Fixed wrong DOM id link in the push_event javascript, and reformat it to a sensible linelength

--- app/helpers/event_rendering_helper.rb
+++ app/helpers/event_rendering_helper.rb
@@ -294,7 +294,18 @@ module EventRenderingHelper
     project = event.target.project
     commit_link = link_to_if(event.has_commits?, pluralize(event.events.size, 'commit'),
       repo_owner_path(event.target, :project_repository_commits_in_ref_path, project, event.target, ensplat_path(event.data)),
-    :onclick => %{if ($('commits_in_event_#{event.to_param}')) { $('commits_in_event_435240').toggle(); new Ajax.Updater('commits_in_event_#{event.to_param}', '#{commits_event_path(event.to_param)}', {asynchronous:true, evalScripts:true, method:'get'}); return false; }})    
+      :onclick => %{
+         if ($('commits_in_event_#{event.to_param}')) {
+           $('commits_in_event_#{event.to_param}').toggle();
+           new Ajax.Updater('commits_in_event_#{event.to_param}',
+                            '#{commits_event_path(event.to_param)}', {
+                              asynchronous:true,
+                              evalScripts:true,
+                              method:'get'
+                            });
+           return false;
+          }
+      })
     
     action = action_for_event(:event_pushed_n, :commit_link => commit_link) do
       title = repo_title(event.target, project)
