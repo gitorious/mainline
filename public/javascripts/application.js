gitorious/mainline

commit 2e8cdf86459ffd9b05fc8b6ea81c4c68758e70c9

Changed the notation for a commit range from "a..b" to "a-b"
... since we include the commit previous to a.
Also moved some of the code for merge request selection to a "class", made the loading from a
uri hash work nicely.

public/javascripts/application.js
@@ -218,7 +218,7 @@ $(document).ready(function() {
   $("body#merge_requests", function(){
     var spec = Gitorious.ShaSpec.parseLocationHash(document.location.hash);
     if (spec) {
-      spec.summarizeHtml();
+      Gitorious.MergeRequestController.getInstance().loadFromBookmark(spec);
     }
   })
 
@@ -299,24 +299,22 @@ $(document).ready(function() {
         var shaSpec = new Gitorious.ShaSpec();
         shaSpec.addSha(first_commit_sha);
         shaSpec.addSha(last_commit_sha);          
-        document.location.hash = shaSpec.shaSpec();
-        shaSpec.summarizeHtml();
+        Gitorious.MergeRequestController.getInstance().isSelectingShas(shaSpec);
       };
       return jQuery("#merge_request_commit_selector.compact").selectable({
         filter: "li.single_commit",
         stop: function(e, ui) {
-            var sha_spec = new Gitorious.ShaSpec();
-            jQuery("li.ui-selected a", this).each(function() {
-                sha = jQuery(this).attr("data-commit-sha");
-                sha_spec.addSha(sha);
-            });
-          $("#current_shas .label").html("Showing");
-            var mr_diff_url = jQuery("#merge_request_commit_selector")
-              .attr("data-merge-request-version-url");
-            var diff_browser = new Gitorious.DiffBrowser(sha_spec.shaSpec());
+          var sha_spec = new Gitorious.ShaSpec();
+          jQuery("li.ui-selected a", this).each(function() {
+            sha = jQuery(this).attr("data-commit-sha");
+            sha_spec.addSha(sha);
+          });
+          Gitorious.MergeRequestController.getInstance().didSelectShas(sha_spec);
+        },
+        start: function(e, ui) {
+          Gitorious.MergeRequestController.getInstance().willSelectShas();
         },
         selecting: function(e, ui) {
-          $("#current_shas .label").html("Selecting");
           selectingAndUnselecting();
         },
         unselecting: function(e,ui) {
@@ -476,9 +474,9 @@ Gitorious.ShaSpec = function() {
   this.addSha = function(s) {
     this.allShas.push(new Gitorious.Sha(s));
   }
-  // Add shas from a string, eg ff0..bba
+  // Add shas from a string, eg ff0-bba
   this.parseShas = function(shaString) {
-    pair = shaString.split("..");
+    pair = shaString.split("-");
     this.addSha(pair[0]);
     if (pair.length > 1) {
       this.addSha(pair[1]);
@@ -505,12 +503,12 @@ Gitorious.ShaSpec = function() {
   
   this.shaSpec = function() {
     var _specs = this.shaSpecs();
-    return jQuery.map(_specs, function(s){return s.sha()}).join("..");
+    return jQuery.map(_specs, function(s){return s.sha()}).join("-");
   }
   
   this.shortShaSpec = function() {
     var _specs = this.shaSpecs();
-    return jQuery.map(_specs, function(s){ return s.shortSha() }).join("..");
+    return jQuery.map(_specs, function(s){ return s.shortSha() }).join("-");
   }
 
   this.singleCommit = function() {
@@ -578,7 +576,7 @@ Gitorious.DiffBrowser = function(shas)
         jQuery("#merge_request_diff").html(data);
         var shaSpec = new Gitorious.ShaSpec();
         shaSpec.parseShas(shas);
-        shaSpec.summarizeHtml();        
+        Gitorious.MergeRequestController.getInstance().didReceiveVersion(shaSpec);
         Gitorious.setDiffBrowserHunkStateFromCookie();
       }
     },
@@ -588,7 +586,57 @@ Gitorious.DiffBrowser = function(shas)
     }
   });
 }
-    
+
+Gitorious.MergeRequestController = function() {
+  this.willSelectShas = function() {
+    $("#current_shas .label").html("Selecting");
+  }
+  
+  this.didReceiveVersion = function(spec) {
+    console.debug("Did receive version. Spec is " + spec.shaSpec());
+    document.location.hash = spec.shaSpec();
+  }
+
+  this.isSelectingShas = function(spec) {
+    document.location.hash = spec.shaSpec();
+    spec.summarizeHtml();
+  }
+
+  // Loads the requested (from path part of uri) shas and version
+  this.loadFromBookmark = function(spec) {
+    jQuery("li.ui-selected").removeClass("ui-selected");
+    var allShas = jQuery("li.single_commit a").map(function(){
+      return $(this).attr("data-commit-sha");
+    })
+    var currentShas = [];
+    for (var i = allShas.indexOf(spec.firstSha().sha());
+         i <= allShas.indexOf(spec.lastSha().sha()); 
+         i++) {
+      currentShas.push(allShas[i]);
+    }
+    jQuery.each(currentShas, function(ind, sha){
+      jQuery("[data-commit-sha='" + sha + "']").parent().addClass("ui-selected");
+    })
+ }
+
+  this.didSelectShas = function(spec) {
+    $("#current_shas .label").html("Showing");
+    var mr_diff_url = jQuery("#merge_request_commit_selector")
+      .attr("data-merge-request-version-url");
+    var diff_browser = new Gitorious.DiffBrowser(spec.shaSpec());    
+  }    
+}
+
+Gitorious.MergeRequestController.getInstance = function() {
+  if (Gitorious.MergeRequestController._instance) {
+    return Gitorious.MergeRequestController._instance;
+  } else {
+    var result = new Gitorious.MergeRequestController();
+    Gitorious.MergeRequestController._instance = result;
+    return result;
+  }
+}
+
 // Gitorious.Wordwrapper = {
 //   wrap: function(elements) {
 //     elements.each(function(e) {
@@ -629,7 +677,7 @@ function CommitRangeSelector(commitListUrl, targetBranchesUrl, statusElement)
     this.update();
   };
   
-  this.onSourceBranchChange = function(event) {
+  this.onSuccessourceBranchChange = function(event) {
     if (sourceBranch = $('#merge_request_source_branch')) {
       this.sourceBranchSelected(sourceBranch);
     }

app/controllers/merge_request_versions_controller.rb
@@ -35,11 +35,11 @@ class MergeRequestVersionsController < ApplicationController
   
   private
   def commit_range?(shaish)
-    shaish.include?("..")
+    shaish.include?("-")
   end
 
   def extract_range_from_parameter(p)
-    if match = /^([a-z0-9]*)\.\.([a-z0-9]*)$/.match(p)
+    if match = /^([a-z0-9]*)-([a-z0-9]*)$/.match(p)
       Range.new(match[1],match[2])
     else
       p

app/helpers/merge_requests_helper.rb
@@ -148,7 +148,7 @@ module MergeRequestsHelper
     options = {:class => "several_shas"}
     options[:style] = "display:none" if last.blank?
     content_html = content_tag(:span, 'Showing', :class => 'label') + " " +
-      content_tag(:code, first, :class => 'first') + ".." +
+      content_tag(:code, first, :class => 'first') + "-" +
       content_tag(:code, last, :class => 'last')
     content_tag(:div, "#{content_html}", options)
   end

app/models/merge_request_version.rb
@@ -48,7 +48,7 @@ class MergeRequestVersion < ActiveRecord::Base
   def sha_summary(format = :short)
     result = format == :short ? short_merge_base : merge_base_sha
     if !affected_commits.blank?
-      result << ".." +  (format == :short ? affected_commits.last.id_abbrev : affected_commits.last.id)
+      result << "-" +  (format == :short ? affected_commits.last.id_abbrev : affected_commits.last.id)
     end
     result
   end
@@ -62,8 +62,14 @@ class MergeRequestVersion < ActiveRecord::Base
       @repository = repository
     end
 
-    def parent_commit(sha)
-      Grit::Commit.find_all(@repository, sha, {:max_count => 1}).first.parents.first
+    # Returns the sha of +sha+'s parent. If none, return +sha+
+    def parent_commit_sha(sha)
+      first_parent = commit = Grit::Commit.find_all(@repository, sha, {:max_count => 1}).first.parents.first
+      if first_parent.nil?
+        sha
+      else
+        first_parent.id
+      end
     end
     
     def cache_key(first, last=nil)
@@ -73,7 +79,7 @@ class MergeRequestVersion < ActiveRecord::Base
     def commit_diff(first, last, diff_with_previous=false)
       Rails.cache.fetch(cache_key(first,last), :expires_in => 60.minutes) do
         first_commit_sha = if diff_with_previous
-                             parent_commit(first).id
+                             parent_commit_sha(first)
                            else
                              first
                            end

test/functional/merge_request_versions_controller_test.rb
@@ -53,7 +53,7 @@ class MergeRequestVersionsControllerTest < ActionController::TestCase
     context 'Viewing the diff for a series of commits' do
       setup do
         @version.expects(:diffs).with("ffcab".."bacff").returns([])
-        get :show, :id => @version, :commit_shas => "ffcab..bacff"
+        get :show, :id => @version, :commit_shas => "ffcab-bacff"
       end
       should_respond_with :success
       should_not_assign_to(:commit)

commit 88827a9094954d4324a6a982086b7c166aee91c8

Fix weird typo introduced in 2e8cdf86

public/javascripts/application.js
@@ -706,7 +706,7 @@ function CommitRangeSelector(commitListUrl, targetBranchesUrl, statusElement)
     this.update();
   };
   
-  this.onSuccessourceBranchChange = function(event) {
+  this.onSourceBranchChange = function(event) {
     if (sourceBranch = $('#merge_request_source_branch')) {
       this.sourceBranchSelected(sourceBranch);
     }