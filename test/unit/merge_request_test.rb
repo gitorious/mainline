dc7aade6fa82d946383b915c5b99ddead35975fe
Johan Sørensen <johan@johansorensen.com>
2009-11-18 14:20:22 +0100

    Only notify reviewers about new merge requests

--- test/unit/merge_request_test.rb
+++ test/unit/merge_request_test.rb
@@ -34,12 +34,12 @@ class MergeRequestTest < ActiveSupport::TestCase
     @merge_request.stubs(:commits_for_selection).returns(commits)
     assert @merge_request.pending_acceptance_of_terms?
   end
-  
+
   should_validate_presence_of :user, :source_repository, :target_repository
   should_validate_presence_of :summary
-  
+
   should_have_many :comments
-  
+
   should "email all committers in the target_repository after the terms are accepted" do
     assert_incremented_by(Message, :count, 1) do
       mr = @merge_request.clone
@@ -50,7 +50,7 @@ class MergeRequestTest < ActiveSupport::TestCase
       mr.terms_accepted
     end
   end
-  
+
   should 'calculate the merge base between target branch and self' do
     repo = mock("Git repo")
     git = mock("Git")
@@ -60,14 +60,14 @@ class MergeRequestTest < ActiveSupport::TestCase
       @merge_request.target_branch, "refs/merge-requests/#{@merge_request.id}").returns("ffcaabd\n")
     assert_equal 'ffcaabd', @merge_request.calculate_merge_base
   end
-  
+
   should 'create a version with the merge base' do
     @merge_request.expects(:calculate_merge_base).returns('ffcaabd')
     version = @merge_request.create_new_version
     assert_equal 1, version.version
     assert_equal "ffcaabd", version.merge_base_sha
   end
-  
+
   should 'generate valid version numbers for its version' do
     version = @merge_request.build_new_version
     assert version.save
@@ -76,7 +76,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     assert version.save
     assert_equal 2, version.version
   end
-  
+
   should 'send a MQ message when being confirmed by the user' do
     p = proc {@merge_request.confirmed_by_user}
     message = find_message_with_queue_and_regexp('/queue/GitoriousMergeRequestCreation', /.*/) {p.call}
@@ -84,12 +84,16 @@ class MergeRequestTest < ActiveSupport::TestCase
   end
 
   should "send a message to all the subscribers when confirmed by the user" do
-    # -1 because the merge_request creator itself is a committer in this case
-    assert_difference("Message.count", @merge_request.target_repository.committers.count-1) do
+    assert_equal 1, @merge_request.target_repository.committerships.count
+    cs =  @merge_request.target_repository.committerships.first
+    cs.build_permissions(:review); cs.save!
+    @merge_request.user = users(:mike)
+    @merge_request.save
+    assert_difference("Message.count", @merge_request.target_repository.reviewers.size) do
       @merge_request.confirmed_by_user
     end
   end
-  
+
   should "not send messages the subscribers when confirmed by the user, if the target_repo has it turned off" do
     @merge_request.target_repository.update_attribute(:notify_committers_on_new_merge_request, false)
     assert_no_difference("Message.count") do
@@ -101,7 +105,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     setup do
       @project = @merge_request.target_repository.project
     end
-    
+
     should "find the default state" do
       assert_nil @merge_request.default_status
       MergeRequestStatus.create_defaults_for_project(@project)
@@ -136,18 +140,18 @@ class MergeRequestTest < ActiveSupport::TestCase
       assert @merge_request.ready?
     end
   end
-  
+
   should 'be able to update from a push event' do
     @merge_request.expects(:push_new_branch_to_tracking_repo).once
     @merge_request.update_from_push!
     @merge_request.reload
   end
-  
+
   should "return its target repository's tracking repository" do
     tracking_repo = @merge_request.target_repository.create_tracking_repository
     assert_equal tracking_repo, @merge_request.tracking_repository
   end
-  
+
   should 'create a new version with the merge base between target branch and self' do
     @merge_request.expects(:calculate_merge_base).returns('ff0')
     version = @merge_request.create_new_version
@@ -170,13 +174,13 @@ class MergeRequestTest < ActiveSupport::TestCase
     assert_equal "refs/merge-requests/#{@merge_request.id}/1", @merge_request.merge_branch_name(1)
     assert_equal "refs/merge-requests/#{@merge_request.id}/#{version.version}", @merge_request.merge_branch_name(:current)
   end
-  
+
   should 'push new branch to tracking repo' do
     tracking_repo = mock
     tracking_repo.stubs(:full_repository_path).returns("/tmp/foo.git")
     @merge_request.stubs(:tracking_repository).returns(tracking_repo)
     repo = mock("Target repository")
-    repo.expects(:push).once.with({:timeout => false}, 
+    repo.expects(:push).once.with({:timeout => false},
       @merge_request.tracking_repository.full_repository_path,
       "refs/merge-requests/#{@merge_request.id}:refs/merge-requests/#{@merge_request.id}/#{@merge_request.next_version_number}")
     git = mock
@@ -187,12 +191,12 @@ class MergeRequestTest < ActiveSupport::TestCase
       @merge_request.push_new_branch_to_tracking_repo
     end
   end
-  
+
   should "has a closed? status" do
     @merge_request.status = MergeRequest::STATUS_CLOSED
     assert @merge_request.closed?, '@merge_request.closed? should be true'
   end
-  
+
   should "has a open? status" do
     @merge_request.status = MergeRequest::STATUS_OPEN
     assert @merge_request.open?, '@merge_request.open? should be true'
@@ -215,20 +219,20 @@ class MergeRequestTest < ActiveSupport::TestCase
     assert @merge_request.commit_merged?('ff0')
     assert !@merge_request.commit_merged?('ff1')
   end
-  
+
   should "has a statuses class method" do
     assert_equal MergeRequest::STATUS_PENDING_ACCEPTANCE_OF_TERMS, MergeRequest.statuses["Pending"]
     assert_equal MergeRequest::STATUS_OPEN, MergeRequest.statuses["Open"]
     assert_equal MergeRequest::STATUS_CLOSED, MergeRequest.statuses["Closed"]
   end
-  
+
   should "has a status_string" do
     MergeRequest.statuses.each do |k,v|
       @merge_request.status = v
       assert_equal k.downcase, @merge_request.status_string
     end
   end
-  
+
   should "knows who can resolve itself" do
     assert @merge_request.resolvable_by?(users(:johan))
     @merge_request.target_repository.committerships.create!({
@@ -247,18 +251,18 @@ class MergeRequestTest < ActiveSupport::TestCase
     assert @merge_request.resolvable_by?(creator), "not resolvable by creator"
     assert !@merge_request.resolvable_by?(users(:moe))
   end
-  
+
   should "have a working resolvable_by? together with fucktard authentication systems" do
     assert !@merge_request.resolvable_by?(:false)
   end
-  
+
   should "count open merge_requests" do
     mr = @merge_request.clone
     mr.status = MergeRequest::STATUS_CLOSED
     mr.save
     assert_equal 2, MergeRequest.count_open
   end
-  
+
   should 'have a transition from pending to open' do
     mr = @merge_request.clone
     assert mr.pending_acceptance_of_terms?
@@ -269,7 +273,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     assert_equal 'valid_version_sha', mr.contribution_agreement_version
     assert_equal 'Thank you for your contribution', mr.contribution_notice
   end
-  
+
   should 'not be set to open if OAuth validation fails' do
     mr = @merge_request.clone
     mr.oauth_token = "key"
@@ -277,46 +281,46 @@ class MergeRequestTest < ActiveSupport::TestCase
     mr.oauth_consumer.valid_oauth_credentials=({:key => 'key', :secret => 'secret'})
     assert !mr.open?
   end
-  
+
   should 'require signoff when target repository requires it' do
     mr = @merge_request.clone
     assert mr.acceptance_of_terms_required?
   end
-  
+
   should 'not require signoff when target repository does not require so' do
     mr = MergeRequest.new(:source_repository => repositories(:johans), :target_repository => repositories(:johans2), :ending_commit => '00ffcc')
     assert !mr.acceptance_of_terms_required?
   end
-  
+
   should "it defaults to master for the source_branch" do
     mr = MergeRequest.new
     assert_equal "master", mr.source_branch
     mr.source_branch = "foo"
     assert_equal "foo", mr.source_branch
   end
-  
+
   should "it defaults to master for the target_branch" do
     mr = MergeRequest.new
     assert_equal "master", mr.target_branch
     mr.target_branch = "foo"
     assert_equal "foo", mr.target_branch
   end
-  
+
   should "has a source_name" do
     @merge_request.source_branch = "foo"
     assert_equal "#{@merge_request.source_repository.name}:foo", @merge_request.source_name
   end
-  
+
   should "has a target_name" do
     @merge_request.target_branch = "foo"
     assert_equal "#{@merge_request.target_repository.name}:foo", @merge_request.target_name
   end
-  
+
   should "have an empty set of target branches, if the target_repository is nil" do
     @merge_request.target_repository = nil
     assert_equal [], @merge_request.target_branches_for_selection
   end
-  
+
   should "have a set of target branches" do
     repo = repositories(:johans)
     @merge_request.target_repository = repo
@@ -324,23 +328,23 @@ class MergeRequestTest < ActiveSupport::TestCase
     repo.stubs(:git).returns(grit)
     assert_equal grit.heads, @merge_request.target_branches_for_selection
   end
-  
+
   context "with specific starting and ending commits" do
     setup do
       @merge_request.ending_commit = '6823e6622e1da9751c87380ff01a1db1'
     end
-    
+
     should "not blow up if there's no target repository" do
       mr = MergeRequest.new
       assert_nothing_raised do
         assert_equal [], mr.commits_for_selection
       end
     end
-    
+
     should "suggest relevant commits to be merged" do
       assert_equal(4, @merge_request.commits_for_selection.size)
     end
-    
+
     should 'show a list of potential commits' do
       assert_equal 3, @merge_request.potential_commits.size
     end
@@ -351,20 +355,20 @@ class MergeRequestTest < ActiveSupport::TestCase
       assert_nil @merge_request.version_number(4)
       assert_equal 3, @merge_request.version_number(3).version
     end
-    
+
     should 'use the real commits if the target branch has been updated' do
       @merge_request.stubs(:calculate_merge_base).returns('ff0')
       @merge_request.create_new_version
       @merge_request.expects(:commit_diff_from_tracking_repo).once
       @merge_request.commits_to_be_merged
     end
-    
+
     should 'return an empty list if the target branch has not been updated' do
       @merge_request.versions.destroy_all
       assert_equal 4, @merge_request.commits_for_selection.size
       assert_equal 3, @merge_request.commits_to_be_merged.size
     end
-    
+
     should 'know if the specified commit exists in the source repository' do
       source_git = mock('Source repository Git repo')
       source_git.expects(:commit).with('ff00ddca').returns(nil)
@@ -376,21 +380,21 @@ class MergeRequestTest < ActiveSupport::TestCase
       assert @merge_request.ending_commit_exists?
     end
   end
-  
+
   context 'The state machine' do
     setup {@merge_request = merge_requests(:moes_to_johans)}
-    
+
     should 'allow transition to other states as long as it is not rejected or merged' do
       @merge_request.open!
       assert @merge_request.can_transition_to?('close')
     end
-    
+
     should 'not allow transition to other states when rejected' do
       @merge_request.open!
       @merge_request.close!
       assert !@merge_request.can_transition_to?('open')
     end
-    
+
     should 'optionally take a block when performing a transition' do
       @merge_request.open!
       @merge_request.expects(:foo=).once
@@ -399,7 +403,7 @@ class MergeRequestTest < ActiveSupport::TestCase
       end
       assert status_changed
     end
-    
+
     should 'allow admin users to re-open' do
       @user = users(:johan)
       @merge_request.open
@@ -409,7 +413,7 @@ class MergeRequestTest < ActiveSupport::TestCase
       assert @merge_request.reopen_with_user(@user)
       assert @merge_request.open?
     end
-    
+
     should 'not allow non-admin users to re-open' do
       @user = users(:moe)
       @merge_request.open
@@ -419,19 +423,19 @@ class MergeRequestTest < ActiveSupport::TestCase
       assert !@merge_request.reopen_with_user(@user)
       assert !@merge_request.open?
     end
-    
+
     should 'return false from its transition_to method if the state change is disallowed' do
       @merge_request.stubs(:can_transition_to?).returns(false)
       status_changed = @merge_request.transition_to(MergeRequest::STATUS_OPEN)
       assert !status_changed
     end
-    
+
     should 'deliver a status update to the user who initiated it' do
       assert_incremented_by(@merge_request.user.received_messages, :count, 1) do
         @merge_request.deliver_status_update(users(:moe))
       end
     end
-    
+
     should 'nullify associated messages when deleted' do
       @merge_request.deliver_status_update(users(:moe))
       message = @merge_request.user.received_messages.last
@@ -439,7 +443,7 @@ class MergeRequestTest < ActiveSupport::TestCase
       message.reload
       assert_nil message.notifiable
     end
-    
+
     should_eventually 'provide a hash of labels and values for possible next states' do
       @merge_request.status = MergeRequest::STATUS_VERIFYING
       assert_equal({'Merged' => 'merge', 'Rejected' => 'reject'}, @merge_request.possible_next_states_hash)
@@ -452,7 +456,7 @@ class MergeRequestTest < ActiveSupport::TestCase
       @merge_request.status = MergeRequest::STATUS_MERGED
       assert_equal({}, @merge_request.possible_next_states_hash)
     end
-    
+
     should_eventually 'have a pseudo-open status' do
       [MergeRequest::STATUS_OPEN].each do |s|
         @merge_request.status = s
@@ -462,7 +466,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     end
   end
 
-  
+
   context 'Compatibility with existing records' do
     setup do
       @source_repo = repositories(:johans2)
@@ -476,24 +480,24 @@ class MergeRequestTest < ActiveSupport::TestCase
           :proposal => 'Please, mister postman'
         })
     end
-    
+
     should 'require ending_commit for new records' do
       assert !@merge_request.save
       assert_not_nil @merge_request.errors.on(:ending_commit)
     end
-    
+
     should 'not consider a missing ending_commit a show stopper on update' do
       @merge_request.save(false)
       @merge_request.proposal = 'Yikes'
       assert @merge_request.save
     end
   end
-  
+
   context 'Last updated by' do
     setup do
       @merge_request = merge_requests(:moes_to_johans_open)
     end
-    
+
     should 'initially be the user' do
       assert_equal users(:johan), @merge_request.updated_by
     end
@@ -501,7 +505,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     should 'have a setter and getter' do
       @merge_request.updated_by = users(:mike)
       assert_equal users(:mike), @merge_request.updated_by
-    end    
+    end
   end
 
   should 'have a recently_added? method' do
@@ -511,7 +515,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     @merge_request.created_at = 1.minute.ago
     assert @merge_request.recently_created?
   end
-  
+
   context "from_filter" do
     setup do
       @repo = repositories(:johans)
@@ -519,20 +523,20 @@ class MergeRequestTest < ActiveSupport::TestCase
       merge_requests(:moes_to_johans).update_attribute(:status, MergeRequest::STATUS_OPEN)
       MergeRequestStatus.create_defaults_for_project(@repo.project)
     end
-    
+
     should "default to open merge-requests" do
       merge_requests(:moes_to_johans).update_attribute(:status_tag, 'Closed')
       merge_requests(:moes_to_johans_open).update_attribute(:status_tag, 'Open')
       assert !@repo.merge_requests.from_filter(nil).include?(merge_requests(:moes_to_johans))
       assert_equal [merge_requests(:moes_to_johans_open)], @repo.merge_requests.from_filter(nil)
     end
-    
+
     should "fall back to using named_scope on other filter name" do
       merge_requests(:moes_to_johans).update_attribute(:status_tag, 'kittens')
       assert !@repo.merge_requests.from_filter("kittens").include?(merge_requests(:moes_to_johans_open))
       assert_equal [merge_requests(:moes_to_johans)], @repo.merge_requests.from_filter("kittens")
     end
-    
+
     should "find closed merge-requests" do
       merge_requests(:moes_to_johans).update_attribute(:status_tag, 'Closed')
       assert !@repo.merge_requests.from_filter("Closed").include?(merge_requests(:moes_to_johans_open))
@@ -542,12 +546,12 @@ class MergeRequestTest < ActiveSupport::TestCase
 
   context 'As XML' do
     setup {@merge_request = merge_requests(:moes_to_johans_open)}
-    
+
     should 'not include confidential information' do
       assert !@merge_request.to_xml.include?('<contribution-agreement-version')
       assert !@merge_request.to_xml.include?('<oauth-secret')
     end
-    
+
     should 'include enough information for our purposes' do
       assert_match(/<status>#{@merge_request.status_tag}<\/status>/,
         @merge_request.to_xml)
@@ -560,12 +564,12 @@ class MergeRequestTest < ActiveSupport::TestCase
 
     end
   end
-  
+
   context 'Pushing changes to the merge request repository' do
     setup do
       @merge_request = merge_requests(:moes_to_johans)
     end
-    
+
     should 'send a push command from the source repository to the tracking repository' do
       merge_request_repo = @merge_request.target_repository.create_tracking_repository
       merge_request_repo_path = merge_request_repo.full_repository_path
@@ -620,7 +624,7 @@ class MergeRequestTest < ActiveSupport::TestCase
       MergeRequestStatus.create_defaults_for_project(
         @merge_request.target_repository.project)
     end
-    
+
     should 'cascade to the actual state machine with given states' do
       @merge_request.status_tag = 'closed'
       assert @merge_request.reload.closed?
@@ -690,7 +694,7 @@ class MergeRequestTest < ActiveSupport::TestCase
 
   context 'Migration of reason/status to comment/status_tag' do
     setup {@merge_request = merge_requests(:moes_to_johans_open)}
-      
+
     should 'simply set the status tag if no reason exists' do
       @merge_request.migrate_to_status_tag
       assert_equal 'Open', @merge_request.reload.status_tag.to_s
@@ -711,7 +715,7 @@ class MergeRequestTest < ActiveSupport::TestCase
     setup do
       @merge_request = merge_requests(:moes_to_johans_open)
     end
-    
+
     should "send a message when being destroyed" do
       deletion_proc = proc{ @merge_request.destroy }
       msg = find_message_with_queue_and_regexp('/queue/GitoriousMergeRequestBackend', /.*/) {
