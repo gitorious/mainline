a3af2cbfb50d1bb25ddf850e12ce065f3398b9f3
Johan Sørensen <johan@johansorensen.com>
2009-11-19 15:41:50 +0100

    Call timeago() after each ajax call
    So any loaded html will have the proper timestamps set

--- public/javascripts/jquery.gitorious_extensions.js
+++ public/javascripts/jquery.gitorious_extensions.js
@@ -15,12 +15,13 @@
 #
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#-- 
+#--
 */
 
 // Make JQuery work with Rails' respond_to
 jQuery.ajaxSetup({
-  'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript") }
+    'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript"); },
+    'complete': function() { jQuery('abbr.timeago').timeago(); }
 });
 
 jQuery.fn.highlightSelectedLines = function() {
