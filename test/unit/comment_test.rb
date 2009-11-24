c7b03c65858c22c238ef42b64d3f65e33b91dd8c
Johan Sørensen <johan@johansorensen.com>
2009-11-24 09:24:57 +0100

    Add sanity-check assertion

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -120,6 +120,7 @@ class CommentTest < ActiveSupport::TestCase
     should 'not change the state of its target unless the user can resolve it' do
       @merge_request = merge_requests(:moes_to_johans_open)
       @merge_request.update_attribute(:status_tag, 'Before')
+      assert !@merge_request.resolvable_by?(users(:moe))
       @comment = @merge_request.comments.new(:body => 'PDI', :project => projects(:johans))
       @comment.state = 'After'
       @comment.user = users(:moe)
