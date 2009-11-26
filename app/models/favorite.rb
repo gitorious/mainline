0cee27f7c42ebe44f5601f78ba4b804709acfe82
Marius Mathiesen <marius@shortcut.no>
2009-11-26 10:25:39 +0100

    Improve the favorite/favoritable model:
    - Validations
    - Extract watchable logic into module

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,5 @@ class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :watchable, :polymorphic => true
   validates_presence_of :user_id, :watchable_id, :watchable_type
+  validates_uniqueness_of :user_id, :scope => [:watchable_id, :watchable_type]
 end
