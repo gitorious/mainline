a65dd0971f420e65678dd4bf154792050a45f7e0
Peter Kjellerstedt <pkj@axis.com>
2010-06-02 17:23:38 +0200

    Allow contact page to be viewed without being logged in
    It is a bit harsh to require one to be logged in to be able to view the
    contact information; especially if one is having trouble logging in...

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -21,7 +21,7 @@
 #++
 
 class SiteController < ApplicationController
-  skip_before_filter :public_and_logged_in, :only => [:index, :about, :faq]
+  skip_before_filter :public_and_logged_in, :only => [:index, :about, :faq, :contact]
   before_filter :login_required, :only => [:dashboard]
   renders_in_site_specific_context :except => [:about, :faq, :contact]
   renders_in_global_context :only => [:about, :faq, :contact]
