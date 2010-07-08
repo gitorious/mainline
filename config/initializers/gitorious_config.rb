d89fa8378b056e9f8edac5fffe93eeae865b1c67
Marius Mathiesen <marius@shortcut.no>
2010-07-08 14:09:37 +0200

    Warn about host/request settings that will not work
    - gitorious.yml needs to have a fqdn for sessions to work
    - Logging in using a different host name than that defined in gitorious.yml will not work
    
    Warn about both in the Rails log

--- config/initializers/gitorious_config.rb
+++ config/initializers/gitorious_config.rb
@@ -7,3 +7,10 @@ GitoriousConfig["locale"] = "en" if GitoriousConfig["locale"].nil?
 # set global locale
 I18n.default_locale = GitoriousConfig["locale"]
 I18n.locale         = GitoriousConfig["locale"]
+
+require "subdomain_validation"
+GitoriousConfig.extend(SubdomainValidation)
+
+if !GitoriousConfig.valid_subdomain?
+  Rails.logger.warn "Invalid subdomain name #{GitoriousConfig['gitorious_host']}. Session cookies will not work!"
+end
