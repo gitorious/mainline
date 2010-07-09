8b16aa5c600528691e59262177dca2e773030f24
Christian Johansen <christian@shortcut.no>
2010-07-09 13:55:16 +0200

    Clean up remaining JavaScripts   - Comply with JsLint   - Kill implicit globals

--- public/javascripts/gitorious/live_search.js
+++ public/javascripts/gitorious/live_search.js
@@ -1,51 +1,59 @@
 /*
-  #--
-  #   Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies)
-  #
-  #   This program is free software: you can redistribute it and/or modify
-  #   it under the terms of the GNU Affero General Public License as published by
-  #   the Free Software Foundation, either version 3 of the License, or
-  #   (at your option) any later version.
-  #
-  #   This program is distributed in the hope that it will be useful,
-  #   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  #   GNU Affero General Public License for more details.
-  #
-  #   You should have received a copy of the GNU Affero General Public License
-  #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  #-- 
+#--
+#   Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies)
+#   Copyright (C) 2010 Christian Johansen <christian@shortcut.no>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-- 
 */
+/*jslint eqeqeq: false, plusplus: false, onevar: false*/
+/*global $, window*/
+
 /*
   Creating a jquery extension
 */
-jQuery.fn.liveSearch = function(backend, options) {
+$.fn.liveSearch = function (backend, opt) {
     // Set some options
-    options = jQuery.extend({
+    var options = $.extend({
         resultContainer: ".live-search-results",
         backend: this.backend,
         waitingClass: "waiting",
         resourceUri: "/foo/bar",
         delay: 250,
         itemCass: "result",
-        renderer: {render: function (obj) {
-            return jQuery("<li class=\"item\">" + obj.name + "</li>")}}
-    }, options);
+        renderer: {
+            render: function (obj) {
+                return $("<li class=\"item\">" + obj.name + "</li>");
+            }
+        }
+    }, opt);
 
     var element = this, // Keep this, ie. where this is started, as element
-    container = element.find(options.resultContainer),  // Where the results go
-    timer,  // Timing to avoid parallell searches
-    previous,  // The last search term
-    input = element.find("input[type=text]"),  
-    uri = options.resourceUri + "?" + input.attr("name") + "=",
-    resetter,
-    noResults = element.find(".no-results-found");  
+        container = element.find(options.resultContainer),  // Where the results go
+        timer,  // Timing to avoid parallell searches
+        previous,  // The last search term
+        input = element.find("input[type=text]"),  
+        uri = options.resourceUri + "?" + input.attr("name") + "=",
+        resetter,
+        noResults = element.find(".no-results-found");  
     
     // Create the container element if it doesn't exist
     if (container.length === 0) {
-        container = jQuery('<ul class="live-search-results"></ul>');
+        container = $('<ul class="live-search-results"></ul>');
         container.appendTo(element);
     }
+
     container.hide();
     noResults.hide();
 
@@ -55,32 +63,34 @@ jQuery.fn.liveSearch = function(backend, options) {
 
     // Stop the timer
     function stopTimer() {
-        return timer && (timer = clearTimeout(timer));
+        return timer && (timer = window.clearTimeout(timer));
     }
 
     /*
       The logic behind it all. This object is returned, so it can be manipulated in tests
     */
-    publicApi = {
-        
+    var publicApi = {
         // Receive a search, queue it for some time, then perform the search
-        queueSearch: function(phrase) {
+        queueSearch: function (phrase) {
             if (phrase === "") {
                 this.reset();
                 return;
             }
+
             if (new RegExp("^" + previous + "$", "i").test(phrase)) {
                 return;
             }
+
             stopTimer();
             previous = phrase;
-            timer = setTimeout(function (){
+
+            timer = window.setTimeout(function () {
                 return this.performSearch(phrase);
             }.bind(this), options.delay);
         },
 
         // Actually call out to the backend and perform the search
-        performSearch: function(phrase) {
+        performSearch: function (phrase) {
             stopTimer();
             previous = phrase;
             element.addClass(options.waitingClass);
@@ -89,31 +99,35 @@ jQuery.fn.liveSearch = function(backend, options) {
         },
 
         // When we receive a result, populate this into the dom
-        populate: function(result, phrase) {
-            if (typeof result != "object")
+        populate: function (result, phrase) {
+            if (typeof result != "object") {
                 throw new TypeError("Expected an object");
+            }
 
-            if (options.onDisplay)
+            if (options.onDisplay) {
                 options.onDisplay();
+            }
 
             resetter.show();
             element.removeClass(options.waitingClass);
             container.html("").show();
+
             if (result.length < 1) {
                 noResults.show();
             } else {
                 noResults.hide();
-                jQuery.each(result, function (i, obj) {
-                    markup = options.renderer.render(obj);
+                $.each(result, function (i, obj) {
+                    var markup = options.renderer.render(obj);
                     markup.appendTo(container);
                 });
             }
         },
 
         // Remove the search results, call into onReset 
-        reset: function() {
-            if (options.onReset)
+        reset: function () {
+            if (options.onReset) {
                 options.onReset();
+            }
 
             input.val("");
             resetter.hide();
@@ -122,21 +136,20 @@ jQuery.fn.liveSearch = function(backend, options) {
     };
 
     // Hook into the events in the DOM
-    (function (){
-        var handler = function(e) {
+    (function () {
+        var handler = function (e) {
             return publicApi.queueSearch(input.val());
-        }
+        };
         
-        var resetFunc = function(e) {
+        var resetFunc = function (e) {
             return publicApi.reset();
-        }
+        };
+
         input.keyup(handler);    
         input.focus(handler);
         resetter.click(resetFunc);
         element.submit(handler);
-    })();
+    }());
 
     return publicApi; // Return the API itself so we can play around with it
-
-}
-
+};
--- public/javascripts/gitorious/merge_requests.js
+++ public/javascripts/gitorious/merge_requests.js
@@ -2,6 +2,7 @@
 #--
 #   Copyright (C) 2007-2009 Johan Sørensen <johan@johansorensen.com>
 #   Copyright (C) 2009 Marius Mathiesen <marius.mathiesen@gmail.com>
+#   Copyright (C) 2010 Christian Johansen <christian@shortcut.no>
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU Affero General Public License as published by
@@ -17,41 +18,54 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-- 
 */
+/*jslint eqeqeq: false, plusplus: false, onevar: false*/
+/*global Gitorious, NotificationCenter, $, document*/
+
 var diffBrowserCompactCommitSelectable;
-$(document).ready(function() {
-    $("body#merge_requests", function(){
+
+if (!this.Gitorious) {
+    this.Gitorious = {};
+}
+
+$(document).ready(function () {
+    $("body#merge_requests", function () {
         var spec = Gitorious.ShaSpec.parseLocationHash(document.location.hash);
+
         if (spec) {
-          Gitorious.MergeRequestController.getInstance().loadFromBookmark(spec);
+            Gitorious.MergeRequestController.getInstance().loadFromBookmark(spec);
         }
     });
 
     // toggling of diffs in diff browsers
-    $('.file-diff .header').live("click", function(event) {
+    $('.file-diff .header').live("click", function (event) {
         var hunksContainer = $(this).next();
+
         if (hunksContainer.is(":visible")) {
-          $(this).removeClass("open").addClass("closed");
-          hunksContainer.slideUp();
+            $(this).removeClass("open").addClass("closed");
+            hunksContainer.slideUp();
         } else {
-          $(this).removeClass("closed").addClass("open");
-          hunksContainer.slideDown();
+            $(this).removeClass("closed").addClass("open");
+            hunksContainer.slideDown();
         }
+
         event.preventDefault();
     });
-    $(".file-diff-controls a#expand-all").live("click", function(e){
+
+    $(".file-diff-controls a#expand-all").live("click", function (e) {
         var container = $(this).parent().parent().parent();
         var cookiePrefix = $(this).attr("gts:cookie-prefix") || 'generic';
         container.find('.file-diff .header').removeClass("closed").addClass("open");
         container.find('.diff-hunks:hidden').show();
-        $.cookie(cookiePrefix + "-diff-hunks-state", "expanded", { expires:365 });
+        $.cookie(cookiePrefix + "-diff-hunks-state", "expanded", { expires: 365 });
         e.preventDefault();
     });
-    $(".file-diff-controls a#collapse-all").live("click", function(e){
+
+    $(".file-diff-controls a#collapse-all").live("click", function (e) {
         var container = $(this).parent().parent().parent();
         var cookiePrefix = $(this).attr("gts:cookie-prefix") || 'generic';
         container.find('.file-diff .header').removeClass("open").addClass("closed");
         container.find('.diff-hunks').hide();
-        $.cookie(cookiePrefix + "-diff-hunks-state", "collapsed", { expires:365 });
+        $.cookie(cookiePrefix + "-diff-hunks-state", "collapsed", { expires: 365 });
         e.preventDefault();
     });
 
@@ -61,62 +75,73 @@ $(document).ready(function() {
 
     // Merge request selection of branches, compact mode
     // wrapped in a function so we can reuse it when we load another version
-    diffBrowserCompactCommitSelectable = function() {
-      var selectingAndUnselecting = function() {
-        var commits = $("li.ui-selecting a");
-        if (!commits[0]) return true;
-        var first_commit_sha = $(commits[0]).attr("data-commit-sha");
-        var last_commit_sha = $(commits[commits.length - 1]).attr("data-commit-sha");
-        
-        var shaSpec = new Gitorious.ShaSpec();
-        shaSpec.addSha(first_commit_sha);
-        shaSpec.addSha(last_commit_sha); 
+    diffBrowserCompactCommitSelectable = function () {
+        var selectingAndUnselecting = function () {
+            var commits = $("li.ui-selecting a");
 
-        Gitorious.MergeRequestController.getInstance().isSelectingShas(shaSpec);
-      };
-      return jQuery("#merge_request_commit_selector.compact").selectable({
-        filter: "li.single_commit",
-        stop: function(e, ui) {
-          var sha_spec = new Gitorious.ShaSpec();
-          jQuery("li.ui-selected a", this).each(function() {
-            sha = jQuery(this).attr("data-commit-sha");
-            sha_spec.addSha(sha);
-          });
-          Gitorious.MergeRequestController.getInstance().didSelectShas(sha_spec);
-        },
-        start: function(e, ui) {
-          Gitorious.MergeRequestController.getInstance().willSelectShas();
-        },
-        selecting: function(e, ui) {
-          selectingAndUnselecting();
-        },
-        unselecting: function(e,ui) {
-          selectingAndUnselecting();
-        },
-        cancel: ".merge_base"
-      });
-    }
+            if (!commits[0]) {
+                return true;
+            }
+
+            var first_commit_sha = $(commits[0]).attr("data-commit-sha");
+            var last_commit_sha = $(commits[commits.length - 1]).attr("data-commit-sha");
+            
+            var shaSpec = new Gitorious.ShaSpec();
+            shaSpec.addSha(first_commit_sha);
+            shaSpec.addSha(last_commit_sha); 
+
+            Gitorious.MergeRequestController.getInstance().isSelectingShas(shaSpec);
+        };
+
+        return $("#merge_request_commit_selector.compact").selectable({
+            filter: "li.single_commit",
+
+            stop: function (e, ui) {
+                var sha_spec = new Gitorious.ShaSpec();
+                $("li.ui-selected a", this).each(function () {
+                    var sha = $(this).attr("data-commit-sha");
+                    sha_spec.addSha(sha);
+                });
+
+                Gitorious.MergeRequestController.getInstance().didSelectShas(sha_spec);
+            },
+
+            start: function (e, ui) {
+                Gitorious.MergeRequestController.getInstance().willSelectShas();
+            },
+
+            selecting: function (e, ui) {
+                selectingAndUnselecting();
+            },
+
+            unselecting: function (e, ui) {
+                selectingAndUnselecting();
+            },
+
+            cancel: ".merge_base"
+        });
+    };
+
     Gitorious.currentMRCompactSelectable = diffBrowserCompactCommitSelectable();
 
     $("#merge_request_version").changableSelection({
-      onChange: function() {
-          var version = $(this).attr("data-mr-version-number");
-          Gitorious.MergeRequestController.getInstance().loadVersion(version);
-//          Gitorious.MergeRequestController.getInstance().versionChanged(version);
-      }
+        onChange: function () {
+            var version = $(this).attr("data-mr-version-number");
+            Gitorious.MergeRequestController.getInstance().loadVersion(version);
+        }
     });
-    
+
     $("#merge_request_current_version ul.compact li.single_commit").hoverBubble();
-    
+
     // Merge request selection of branches, monster mode
-    $("#large_commit_selector_toggler").live("click", function(event) {
+    $("#large_commit_selector_toggler").live("click", function (event) {
         $("#large_commit_selector").slideToggle();
         event.preventDefault();
     });
 
     // Handle selection of multiple commits in the large merge-request commit diff browser
     var previousSelectedCommitRowIndex;
-    $("#large_commit_selector table#commit_table tr input").live("click", function(event) {
+    $("#large_commit_selector table#commit_table tr input").live("click", function (event) {
         var selectedTr = $(this).parents("tr");
         var commitRows = selectedTr.parents("table").find("tr.commit_row");
 
@@ -129,20 +154,20 @@ $(document).ready(function() {
         var firstSelRowIndex = commitRows.indexOf(commitRows.filter(".selected:first")[0]);
         var lastSelRowIndex = commitRows.indexOf(commitRows.filter(".selected:last")[0]);
         var selectedRowIndex = commitRows.indexOf(selectedTr[0]);
-        var markRange = function(start, end) {
+        var markRange = function (start, end) {
             commitRows.slice(start, end + 1).addClass("selected");
         };
 
         // reset selections first
         commitRows.filter(".selected").removeClass("selected");
+
         if (selectedRowIndex === firstSelRowIndex || selectedRowIndex === lastSelRowIndex) {
             selectedTr.addClass("selected");
             return;
         }
 
         if (selectedRowIndex > firstSelRowIndex &&
-            selectedRowIndex < lastSelRowIndex) // in-between
-        {
+            selectedRowIndex < lastSelRowIndex) { // in-between
             if (previousSelectedCommitRowIndex === firstSelRowIndex) {
                 markRange(selectedRowIndex, lastSelRowIndex);
             } else {
@@ -158,14 +183,17 @@ $(document).ready(function() {
     });
 
     // Display a range of commits from the large merge-request commit diff browser
-    $("#show-large-diff-range").live("click", function(event) {
+    $("#show-large-diff-range").live("click", function (event) {
         var selected = $("#large_commit_selector table#commit_table tr.commit_row.selected");
         var spec = new Gitorious.ShaSpec();
         var firstSHA = selected.filter(":first").find("input.merge_to").val();
         var lastSHA = selected.filter(":last").find("input.merge_to").val();
         spec.addSha(firstSHA);
-        if (firstSHA != lastSHA)
-          spec.addSha(lastSHA);
+
+        if (firstSHA != lastSHA) {
+            spec.addSha(lastSHA);
+        }
+
         spec.summarizeHtml();
         var diff_browser = new Gitorious.DiffBrowser(spec.shaSpec());
         $("#large_commit_selector").hide();
@@ -173,7 +201,7 @@ $(document).ready(function() {
     });
 
     // Show a single commit in the large merge-request commit diff browser
-    $("#large_commit_selector #commit_table a.clickable_commit").live("click", function(e){
+    $("#large_commit_selector #commit_table a.clickable_commit").live("click", function (e) {
         var spec = new Gitorious.ShaSpec();
         spec.addSha($(this).attr("data-commit-sha"));
         var diff_browser = new Gitorious.DiffBrowser(spec.shaSpec());
@@ -181,22 +209,20 @@ $(document).ready(function() {
         e.preventDefault();
     });
     
-    jQuery("#current_shas").each(function(){
-        var sha_spec = jQuery(this).attr("data-merge-request-current-shas");
-        diff_browser = new Gitorious.DiffBrowser(sha_spec);
-      }
-    );
+    $("#current_shas").each(function () {
+        var sha_spec = $(this).attr("data-merge-request-current-shas");
+        var diff_browser = new Gitorious.DiffBrowser(sha_spec);
+    });
 
     // Diff commenting
-    $("table tr td.inline_comments a.diff-comment-count").live("click", function(e) {
+    $("table tr td.inline_comments a.diff-comment-count").live("click", function (e) {
         var lineOffsets = $(this).parents("tr").attr("data-line-num-tuple");
         $("#diff-inline-comments-for-" + lineOffsets).slideToggle();
         e.preventDefault();
     });
 
-
     // Clicking on a comment relating to an inline commit comment
-    $(".commentable.comments .inline_comment_link a").live("click", function (){
+    $(".commentable.comments .inline_comment_link a").live("click", function () {
         var comment = $(this).parents("div.comment.inline");
         var path = $(comment).attr("data-diff-path");
         var last_line_number = $(comment).attr("data-last-line-in-diff");
@@ -213,7 +239,7 @@ $(document).ready(function() {
     
     // Clicking on a comment relating to a merge request 
     // version displays the comment in context
-    $("#merge_request_comments .comment.inline .inline_comment_link a").live("click", function() {
+    $("#merge_request_comments .comment.inline .inline_comment_link a").live("click", function () {
         var comment = $(this).parents("div.comment.inline");
         var path = $(comment).attr("data-diff-path");
         var last_line = $(comment).attr("data-last-line-in-diff");
@@ -223,20 +249,23 @@ $(document).ready(function() {
         // Notify the controller of the current version and sha range
         // Add self as listener when this has been completed
         // When this has been completed, remove self as listener
-        var elementInDiff = function(s) {
-          return $(".file-diff[data-diff-path=" + path + "] " + s);
-        }
+        var elementInDiff = function (s) {
+            return $(".file-diff[data-diff-path=" + path + "] " + s);
+        };
+
         var href =  $(this).attr("href");
         var commentSpinner = $("#loading_comment_" + href.split("_")[2]);
         commentSpinner.show();
+
         var jumpToComment = {
-            shaListingCurrent: function(newOrOld) {
+            shaListingCurrent: function (newOrOld) {
                 var c = Gitorious.MergeRequestController.getInstance();
                 var spec = Gitorious.ShaSpec.parseShas(sha_range);
                 c.simulateShaSelection(spec);
                 c.replaceDiffContents(sha_range, this.displayComments, this);
             },
-            displayComments: function(message) {                
+
+            displayComments: function (message) {                
                 var lastLine = elementInDiff("#diff-inline-comments-for-" + last_line);
                 var hunks = elementInDiff(".diff-hunks");
                 hunks.removeClass("closed").addClass("open");
@@ -246,30 +275,29 @@ $(document).ready(function() {
                 this.finish();
                 $.scrollTo(href);
             },
-            finish: function() {
+
+            finish: function () {
                 commentSpinner.hide();
                 NotificationCenter.removeObserver("MergeRequestShaListingUpdated", this);
             }
         };
+
         NotificationCenter.addObserver("MergeRequestShaListingUpdated",
                                        jumpToComment.shaListingCurrent.bind(jumpToComment));
         Gitorious.MergeRequestController.getInstance().versionChanged(version);
+
         return true;
     });
 
-    $("#toggle_inline_comments").live("change", function(){
+    $("#toggle_inline_comments").live("change", function () {
         if ($(this).is(":checked")) {
-          $(".comment.inline").show();
+            $(".comment.inline").show();
         } else {
-          $(".comment.inline").hide();
+            $(".comment.inline").hide();
         }
     });
+
     if ($("#inline_comment_form.commit").length > 0) {
         Gitorious.enableCommenting();
     }
 });
-
-
-if (!Gitorious)
-  var Gitorious = {};
-
