ecdb4044d01b30ac2ef19b4ed71c11a8fbb0cbc4
Christian Johansen <christian@shortcut.no>
2010-08-12 16:53:48 +0200

    Added more tests for repository search and did some refactoring to make it easier to test

--- public/javascripts/gitorious/merge_requests.js
+++ public/javascripts/gitorious/merge_requests.js
@@ -19,7 +19,7 @@
 #-- 
 */
 /*jslint eqeqeq: false, plusplus: false, onevar: false*/
-/*global gitorious, Gitorious, $, document*/
+/*global gitorious, Gitorious, jQuery, document*/
 
 var diffBrowserCompactCommitSelectable;
 
@@ -27,8 +27,8 @@ if (!this.Gitorious) {
     this.Gitorious = {};
 }
 
-$(document).ready(function () {
-    $("body#merge_requests", function () {
+jQuery(document).ready(function () {
+    jQuery("body#merge_requests", function () {
         var spec = Gitorious.ShaSpec.parseLocationHash(document.location.hash);
 
         if (spec) {
@@ -37,54 +37,54 @@ $(document).ready(function () {
     });
 
     // toggling of diffs in diff browsers
-    $('.file-diff .header').live("click", function (event) {
-        var hunksContainer = $(this).next();
+    jQuery('.file-diff .header').live("click", function (event) {
+        var hunksContainer = jQuery(this).next();
 
         if (hunksContainer.is(":visible")) {
-            $(this).removeClass("open").addClass("closed");
+            jQuery(this).removeClass("open").addClass("closed");
             hunksContainer.slideUp();
         } else {
-            $(this).removeClass("closed").addClass("open");
+            jQuery(this).removeClass("closed").addClass("open");
             hunksContainer.slideDown();
         }
 
         event.preventDefault();
     });
 
-    $(".file-diff-controls a#expand-all").live("click", function (e) {
-        var container = $(this).parent().parent().parent();
-        var cookiePrefix = $(this).attr("gts:cookie-prefix") || 'generic';
+    jQuery(".file-diff-controls a#expand-all").live("click", function (e) {
+        var container = jQuery(this).parent().parent().parent();
+        var cookiePrefix = jQuery(this).attr("gts:cookie-prefix") || 'generic';
         container.find('.file-diff .header').removeClass("closed").addClass("open");
         container.find('.diff-hunks:hidden').show();
-        $.cookie(cookiePrefix + "-diff-hunks-state", "expanded", { expires: 365 });
+        jQuery.cookie(cookiePrefix + "-diff-hunks-state", "expanded", { expires: 365 });
         e.preventDefault();
     });
 
-    $(".file-diff-controls a#collapse-all").live("click", function (e) {
-        var container = $(this).parent().parent().parent();
-        var cookiePrefix = $(this).attr("gts:cookie-prefix") || 'generic';
+    jQuery(".file-diff-controls a#collapse-all").live("click", function (e) {
+        var container = jQuery(this).parent().parent().parent();
+        var cookiePrefix = jQuery(this).attr("gts:cookie-prefix") || 'generic';
         container.find('.file-diff .header').removeClass("open").addClass("closed");
         container.find('.diff-hunks').hide();
-        $.cookie(cookiePrefix + "-diff-hunks-state", "collapsed", { expires: 365 });
+        jQuery.cookie(cookiePrefix + "-diff-hunks-state", "collapsed", { expires: 365 });
         e.preventDefault();
     });
 
     // merge request diffing loading indimacator
-    Gitorious.MergeRequestDiffSpinner = $("#merge_request_diff_loading").html();
-    $("#merge_request_diff").html(Gitorious.MergeRequestDiffSpinner);
+    Gitorious.MergeRequestDiffSpinner = jQuery("#merge_request_diff_loading").html();
+    jQuery("#merge_request_diff").html(Gitorious.MergeRequestDiffSpinner);
 
     // Merge request selection of branches, compact mode
     // wrapped in a function so we can reuse it when we load another version
     diffBrowserCompactCommitSelectable = function () {
         var selectingAndUnselecting = function () {
-            var commits = $("li.ui-selecting a");
+            var commits = jQuery("li.ui-selecting a");
 
             if (!commits[0]) {
                 return true;
             }
 
-            var first_commit_sha = $(commits[0]).attr("data-commit-sha");
-            var last_commit_sha = $(commits[commits.length - 1]).attr("data-commit-sha");
+            var first_commit_sha = jQuery(commits[0]).attr("data-commit-sha");
+            var last_commit_sha = jQuery(commits[commits.length - 1]).attr("data-commit-sha");
             
             var shaSpec = new Gitorious.ShaSpec();
             shaSpec.addSha(first_commit_sha);
@@ -93,13 +93,13 @@ $(document).ready(function () {
             Gitorious.MergeRequestController.getInstance().isSelectingShas(shaSpec);
         };
 
-        return $("#merge_request_commit_selector.compact").selectable({
+        return jQuery("#merge_request_commit_selector.compact").selectable({
             filter: "li.single_commit",
 
             stop: function (e, ui) {
                 var sha_spec = new Gitorious.ShaSpec();
-                $("li.ui-selected a", this).each(function () {
-                    var sha = $(this).attr("data-commit-sha");
+                jQuery("li.ui-selected a", this).each(function () {
+                    var sha = jQuery(this).attr("data-commit-sha");
                     sha_spec.addSha(sha);
                 });
 
@@ -124,25 +124,25 @@ $(document).ready(function () {
 
     Gitorious.currentMRCompactSelectable = diffBrowserCompactCommitSelectable();
 
-    $("#merge_request_version").changableSelection({
+    jQuery("#merge_request_version").changableSelection({
         onChange: function () {
-            var version = $(this).attr("data-mr-version-number");
+            var version = jQuery(this).attr("data-mr-version-number");
             Gitorious.MergeRequestController.getInstance().loadVersion(version);
         }
     });
 
-    $("#merge_request_current_version ul.compact li.single_commit").hoverBubble();
+    jQuery("#merge_request_current_version ul.compact li.single_commit").hoverBubble();
 
     // Merge request selection of branches, monster mode
-    $("#large_commit_selector_toggler").live("click", function (event) {
-        $("#large_commit_selector").slideToggle();
+    jQuery("#large_commit_selector_toggler").live("click", function (event) {
+        jQuery("#large_commit_selector").slideToggle();
         event.preventDefault();
     });
 
     // Handle selection of multiple commits in the large merge-request commit diff browser
     var previousSelectedCommitRowIndex;
-    $("#large_commit_selector table#commit_table tr input").live("click", function (event) {
-        var selectedTr = $(this).parents("tr");
+    jQuery("#large_commit_selector table#commit_table tr input").live("click", function (event) {
+        var selectedTr = jQuery(this).parents("tr");
         var commitRows = selectedTr.parents("table").find("tr.commit_row");
 
         if (commitRows.filter(".selected").length === 0) {
@@ -183,8 +183,8 @@ $(document).ready(function () {
     });
 
     // Display a range of commits from the large merge-request commit diff browser
-    $("#show-large-diff-range").live("click", function (event) {
-        var selected = $("#large_commit_selector table#commit_table tr.commit_row.selected");
+    jQuery("#show-large-diff-range").live("click", function (event) {
+        var selected = jQuery("#large_commit_selector table#commit_table tr.commit_row.selected");
         var spec = new Gitorious.ShaSpec();
         var firstSHA = selected.filter(":first").find("input.merge_to").val();
         var lastSHA = selected.filter(":last").find("input.merge_to").val();
@@ -196,65 +196,65 @@ $(document).ready(function () {
 
         spec.summarizeHtml();
         var diff_browser = new Gitorious.DiffBrowser(spec.shaSpec());
-        $("#large_commit_selector").hide();
+        jQuery("#large_commit_selector").hide();
         event.preventDefault();
     });
 
     // Show a single commit in the large merge-request commit diff browser
-    $("#large_commit_selector #commit_table a.clickable_commit").live("click", function (e) {
+    jQuery("#large_commit_selector #commit_table a.clickable_commit").live("click", function (e) {
         var spec = new Gitorious.ShaSpec();
-        spec.addSha($(this).attr("data-commit-sha"));
+        spec.addSha(jQuery(this).attr("data-commit-sha"));
         var diff_browser = new Gitorious.DiffBrowser(spec.shaSpec());
-        $("#large_commit_selector").hide();
+        jQuery("#large_commit_selector").hide();
         e.preventDefault();
     });
     
-    $("#current_shas").each(function () {
-        var sha_spec = $(this).attr("data-merge-request-current-shas");
+    jQuery("#current_shas").each(function () {
+        var sha_spec = jQuery(this).attr("data-merge-request-current-shas");
         var diff_browser = new Gitorious.DiffBrowser(sha_spec);
     });
 
     // Diff commenting
-    $("table tr td.inline_comments a.diff-comment-count").live("click", function (e) {
-        var lineOffsets = $(this).parents("tr").attr("data-line-num-tuple");
-        $("#diff-inline-comments-for-" + lineOffsets).slideToggle();
+    jQuery("table tr td.inline_comments a.diff-comment-count").live("click", function (e) {
+        var lineOffsets = jQuery(this).parents("tr").attr("data-line-num-tuple");
+        jQuery("#diff-inline-comments-for-" + lineOffsets).slideToggle();
         e.preventDefault();
     });
 
     // Clicking on a comment relating to an inline commit comment
-    $(".commentable.comments .inline_comment_link a").live("click", function () {
-        var comment = $(this).parents("div.comment.inline");
-        var path = $(comment).attr("data-diff-path");
-        var last_line_number = $(comment).attr("data-last-line-in-diff");
-        var href = $(this).attr("href");
-        var hunks = $(".file-diff[data-diff-path=" + path + "] .diff-hunks");
+    jQuery(".commentable.comments .inline_comment_link a").live("click", function () {
+        var comment = jQuery(this).parents("div.comment.inline");
+        var path = jQuery(comment).attr("data-diff-path");
+        var last_line_number = jQuery(comment).attr("data-last-line-in-diff");
+        var href = jQuery(this).attr("href");
+        var hunks = jQuery(".file-diff[data-diff-path=" + path + "] .diff-hunks");
         hunks.removeClass("closed").addClass("open");
         hunks.slideDown();
-        var lastLine = $("#diff-inline-comments-for-" + last_line_number);
+        var lastLine = jQuery("#diff-inline-comments-for-" + last_line_number);
         lastLine.slideToggle();
-        Gitorious.DiffBrowser.CommentHighlighter.add($(href));
-        $.scrollTo(href);
+        Gitorious.DiffBrowser.CommentHighlighter.add(jQuery(href));
+        jQuery.scrollTo(href);
         return true;
     });
     
     // Clicking on a comment relating to a merge request 
     // version displays the comment in context
-    $("#merge_request_comments .comment.inline .inline_comment_link a").live("click", function () {
-        var comment = $(this).parents("div.comment.inline");
-        var path = $(comment).attr("data-diff-path");
-        var last_line = $(comment).attr("data-last-line-in-diff");
-        var sha_range = $(comment).attr("data-sha-range");
-        var version = $(comment).attr("data-merge-request-version");
+    jQuery("#merge_request_comments .comment.inline .inline_comment_link a").live("click", function () {
+        var comment = jQuery(this).parents("div.comment.inline");
+        var path = jQuery(comment).attr("data-diff-path");
+        var last_line = jQuery(comment).attr("data-last-line-in-diff");
+        var sha_range = jQuery(comment).attr("data-sha-range");
+        var version = jQuery(comment).attr("data-merge-request-version");
 
         // Notify the controller of the current version and sha range
         // Add self as listener when this has been completed
         // When this has been completed, remove self as listener
         var elementInDiff = function (s) {
-            return $(".file-diff[data-diff-path=" + path + "] " + s);
+            return jQuery(".file-diff[data-diff-path=" + path + "] " + s);
         };
 
-        var href =  $(this).attr("href");
-        var commentSpinner = $("#loading_comment_" + href.split("_")[2]);
+        var href =  jQuery(this).attr("href");
+        var commentSpinner = jQuery("#loading_comment_" + href.split("_")[2]);
         commentSpinner.show();
 
         var jumpToComment = {
@@ -271,9 +271,9 @@ $(document).ready(function () {
                 hunks.removeClass("closed").addClass("open");
                 hunks.slideDown();
                 lastLine.slideToggle();
-                Gitorious.DiffBrowser.CommentHighlighter.add($(href));                
+                Gitorious.DiffBrowser.CommentHighlighter.add(jQuery(href));                
                 this.finish();
-                $.scrollTo(href);
+                jQuery.scrollTo(href);
             },
 
             finish: function () {
@@ -289,15 +289,15 @@ $(document).ready(function () {
         return true;
     });
 
-    $("#toggle_inline_comments").live("change", function () {
-        if ($(this).is(":checked")) {
-            $(".comment.inline").show();
+    jQuery("#toggle_inline_comments").live("change", function () {
+        if (jQuery(this).is(":checked")) {
+            jQuery(".comment.inline").show();
         } else {
-            $(".comment.inline").hide();
+            jQuery(".comment.inline").hide();
         }
     });
 
-    if ($("#inline_comment_form.commit").length > 0) {
+    if (jQuery("#inline_comment_form.commit").length > 0) {
         Gitorious.enableCommenting();
     }
 });
---
+++ test/javascripts/gitorious/repository_search_test.js
@@ -0,0 +1,127 @@
+/*
+  #--
+  #   Copyright (C) 2010 Christian Johansen <christian@shortcut.no>
+  #
+  #   This program is free software: you can redistribute it and/or modify
+  #   it under the terms of the GNU Affero General Public License as published by
+  #   the Free Software Foundation, either version 3 of the License, or
+  #   (at your option) any later version.
+  #
+  #   This program is distributed in the hope that it will be useful,
+  #   but WITHOUT ANY WARRANTY; without even the implied warranty of
+  #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  #   GNU Affero General Public License for more details.
+  #
+  #   You should have received a copy of the GNU Affero General Public License
+  #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  #-- 
+*/
+/*jslint newcap: false, onevar: false, nomen: false*/
+/*global jQuery, gitorious, TestCase, assertSame, assertFalse,
+         assertTrue, assertEquals*/
+
+(function () {
+    var rs = gitorious.repositorySearch;
+
+    TestCase("RepositoryLiveSearchRendererTest", {
+        "test should escape left angle brackets": function () {
+            assertEquals("&lt; oh no &lt;", rs.renderer.escape("< oh no <"));
+        },
+
+        "test should escape right angle brackets": function () {
+            assertEquals("&gt; oh no &gt;", rs.renderer.escape("> oh no >"));
+        },
+
+        "test should escape ampersands": function () {
+            assertEquals("&amp; oh no &amp;", rs.renderer.escape("& oh no &"));
+        },
+
+        "test should escape string": function () {
+            assertEquals("&lt;a href='#'&gt;&lt;span&gt;Hey &amp; hey&lt;/span&gt;&lt;/a&gt;",
+                         rs.renderer.escape("<a href='#'><span>Hey & hey</span></a>"));
+        },
+
+        "test should build markup for repo with name": function () {
+            var result = rs.renderer.render({
+                name: "Gitorious",
+                uri: "/gitorious/mainline",
+                owner_type: "owner",
+                owner_uri: "/users/cjohansen",
+                owner: "Christian Johansen",
+                img: "/images/cj.png"
+            });
+
+            assertEquals(1, result.length);
+            assertTagName("li", result.get(0));
+            assertEquals(2, result.find(">*").length);
+            assertEquals("Gitorious", result.find("div.name a").attr("title"));
+            assertEquals("Gitorious", result.find("div.name a").text());
+            assertEquals("/images/cj.png", result.find("div.owner img").attr("src"));
+            assertEquals("/users/cjohansen", result.find("div.owner a").attr("href"));
+            assertEquals("Christian Johansen", result.find("div.owner a").text());
+        },
+
+        "test should build markup for repo with description": function () {
+            var result = rs.renderer.render({
+                name: "Gitorious",
+                description: "Hi, I'm Gitorious",
+                uri: "/gitorious/mainline",
+                owner_type: "owner",
+                owner_uri: "/users/cjohansen",
+                owner: "Christian Johansen",
+                img: "/images/cj.png"
+            });
+
+            assertEquals("Hi, I'm Gitorious", result.find("div.name a").attr("title"));
+            assertEquals("Gitorious", result.find("div.name a").text());
+            assertEquals("/images/cj.png", result.find("div.owner img").attr("src"));
+            assertEquals("/users/cjohansen", result.find("div.owner a").attr("href"));
+            assertEquals("Christian Johansen", result.find("div.owner a").text());
+        }
+    });
+
+    TestCase("RepositorySearchBackendTest", {
+        "test should call jQuery.getJSON": sinon.test(function () {
+            this.stub(jQuery, "getJSON");
+            var spy = this.spy();
+
+            rs.backend.get("/my/search/", "camera", spy);
+
+            assert(jQuery.getJSON.calledWith("/my/search/camera"));
+        }),
+
+        "test jQuery.getJSON should yield data to callback": sinon.test(function () {
+            this.stub(jQuery, "getJSON");
+            var spy = this.spy();
+            var data = {};
+
+            rs.backend.get("/my/search/", "camera", spy);
+            jQuery.getJSON.getCall(0).args[1](data);
+
+            assertSame(data, spy.getCall(0).args[0]);
+        })
+    });
+
+    TestCase("RepositorySearchCreateTest", {
+        setUp: function () {
+            /*:DOC += <div gts:searchUri="/search" id="repo_search"><input type="text" /></div>*/
+        },
+
+        "test should return search api": function () {
+            var search = rs.create("#repo_search");
+
+            assertFunction(search.queueSearch);
+            assertFunction(search.performSearch);
+        },
+
+        "test should get URL from gts:searchUri attribute": sinon.test(function () {
+            var stub = this.stub(jQuery.fn, "liveSearch");
+
+            var search = rs.create("#repo_search");
+
+            assertEquals(rs.backend, stub.args[0][0]);
+            assertEquals("/search", stub.args[0][1].resourceUri);
+            assertEquals(rs.renderer, stub.args[0][1].renderer)
+        })
+    });
+}());
