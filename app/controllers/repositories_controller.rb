38879a2146f1a4f640893ed4f8f5cdd635ae30e1
Marius Mathiesen <marius@gitorious.org>
2011-01-26 07:54:33 +0100

    Paginate Repositories#index, use ivar for assigned repositories in view.
    This makes it possible to view a large collection of repositories within a project

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -36,7 +36,7 @@ class RepositoriesController < ApplicationController
     if term = params[:filter]
       @repositories = @project.search_repositories(term)
     else
-      @repositories = @owner.repositories.find(:all, :include => [:user, :events, :project])
+      @repositories = @owner.repositories.paginate(:all, :include => [:user, :events, :project], :page => params[:page])
     end
     respond_to do |wants|
       wants.html
