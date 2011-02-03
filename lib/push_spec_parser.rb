e0a6a4f6604efa4a6ab9f83e9bbc92c3a27bd625
Christian Johansen <christian@shortcut.no>
2011-02-03 13:19:27 +0100

    Add documentation

--- lib/push_spec_parser.rb
+++ lib/push_spec_parser.rb
@@ -16,6 +16,25 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
 
+# Parses the line input to git's pre-receive and post-receive hooks:
+# <old-value> SP <new-value> SP <ref-name> LF
+# 
+# The parser basically knows what action this line represents (create, update,
+# delete) and whether its target is tag, a head or (custom for Gitorious) a
+# merge request. The parser also knows the ref-name of the target.
+#
+# Example:
+#
+#     spec = PushSpecParser.new("9b0c1c59c682f9cb908a7aaf28a30b60846237fb",
+#                               "8edf464ae27b6ff8d39600df05ebfbf6c1f1b0d3",
+#                               "refs/heads/master")
+#     spec.head? #=> true
+#     spec.action_update? #=> true
+#     spec.action_create? #=> false
+#     spec.ref_name #=> "master"
+#
+# Refer to unit tests for further examples.
+#
 class PushSpecParser
   attr_reader :from_sha, :to_sha, :ref
 
@@ -53,6 +72,7 @@ class PushSpecParser
     ref.name
   end
 
+  # Internal representation of a single sha.
   class Sha
     def initialize(sha)
       @sha = sha
@@ -63,6 +83,7 @@ class PushSpecParser
     end
   end
 
+  # Internal representation of a Git ref, e.g. res/tags/topic-branch
   class Ref
     attr_reader :name
 
