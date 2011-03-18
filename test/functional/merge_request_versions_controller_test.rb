2a330b57f61ac68ce7028f0ace17fd9c98337562
Christian Johansen <christian@gitorious.org>
2011-03-18 17:12:57 +0100

    Formatting

--- test/functional/merge_request_versions_controller_test.rb
+++ test/functional/merge_request_versions_controller_test.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 #--
+#   Copyright (C) 2011 Gitorious AS
 #   Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies)
 #
 #   This program is free software: you can redistribute it and/or modify
@@ -15,15 +16,13 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
-
-
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + "/../test_helper"
 
 class MergeRequestVersionsControllerTest < ActionController::TestCase
 
   should_render_in_site_specific_context
 
-  context 'Viewing diffs' do
+  context "Viewing diffs" do
     setup do
       @merge_request = merge_requests(:moes_to_johans)
       @merge_request.stubs(:calculate_merge_base).returns("ffac0")
@@ -38,27 +37,29 @@ class MergeRequestVersionsControllerTest < ActionController::TestCase
       Repository.any_instance.stubs(:git).returns(@git)
       MergeRequestVersion.stubs(:find).returns(@version)
     end
-    
-    context 'Viewing the diff for a single commit' do
+
+    context "Viewing the diff for a single commit" do
       setup do
         @version.expects(:diffs).with("ffcab").returns([])
         @git.expects(:commit).with("ffcab").returns(@commit)
         get :show, :id => @version, :commit_shas => "ffcab"
-      end      
+      end
+
       should_respond_with :success
       should_assign_to(:commit, :class => Grit::Commit){ @commit }
     end
-    
-    context 'Viewing the diff for a series of commits' do
+
+    context "Viewing the diff for a series of commits" do
       setup do
         @version.expects(:diffs).with("ffcab".."bacff").returns([])
         get :show, :id => @version, :commit_shas => "ffcab-bacff"
       end
+
       should_respond_with :success
       should_not_assign_to(:commit)
     end
-    
-    context 'Viewing the entire diff' do
+
+    context "Viewing the entire diff" do
       setup do
         @version.expects(:diffs).returns([])
         get :show,  :id => @version
