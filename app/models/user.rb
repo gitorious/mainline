74510fd077bb0dd686fff77c239218c17bf00073
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2011-05-15 19:48:48 -0300

    Remove deprecation warnings from Paperclip usage

--- app/models/user.rb
+++ app/models/user.rb
@@ -102,9 +102,7 @@ class User < ActiveRecord::Base
     Message.find(:all, :conditions => ["sender_id = ? OR recipient_id = ?", self, self])
   end
 
-  Paperclip::Attachment.interpolations['login'] = lambda{|attachment, style|
-    attachment.instance.login.downcase
-  }
+  Paperclip.interpolates('login'){|attachment, style| attachment.instance.login.downcase}
 
   avatar_local_path = '/system/:attachment/:login/:style/:basename.:extension'
   has_attached_file :avatar,
