92bb36be36ef9604719b09a0249f0044e490a692
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2011-06-16 22:13:37 -0300

    Use secure cookies when use_ssl is enabled

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,12 @@
+load File.join(Rails.root, 'config', 'initializers', 'gitorious_config.rb') unless defined? GitoriousConfig
+
 Devise.setup do |config|
   config.stretches = 1
   config.pepper = ""
   config.encryptor = :restful_authentication_sha1
   config.remember_for = 2.weeks
-  # TODO: configure Devise to use secure cookies
+  config.cookie_options = {:httponly => true}
+  config.cookie_options.merge!(:secure => true) if GitoriousConfig['use_ssl']
 end
 
 # Devise 1.0 only: remove when upgrading to Rails 3
