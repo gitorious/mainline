bddeb2c7e2fb57986383a3a3d73bef1c6af82624
Christian Johansen <christian@gitorious.org>
2011-09-05 09:40:37 +0200

    Style clean-up
    - Remove trailing white-space
    - Be consistent about single vs double quotes
    - Improve language (tests have once been automatically converted from RSpec)

--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -16,16 +16,16 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
 
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + "/../test_helper"
 
 class GroupsControllerTest < ActionController::TestCase
-  
+
   should_render_in_global_context
 
   def setup
     @group = groups(:team_thunderbird)
   end
-  
+
   should_enforce_ssl_for(:delete, :avatar)
   should_enforce_ssl_for(:delete, :destroy)
   should_enforce_ssl_for(:get, :edit)
@@ -37,96 +37,99 @@ class GroupsControllerTest < ActionController::TestCase
 
   context "Routing" do
     should "recognizes routes starting with plus as teams/show/<name>" do
-      assert_generates("/+#{@group.to_param}", { :controller => "groups", 
+      assert_generates("/+#{@group.to_param}", { :controller => "groups",
         :action => "show", :id => @group.to_param})
-      assert_recognizes({:controller => "groups", :action => "show", 
+      assert_recognizes({:controller => "groups", :action => "show",
                          :id => @group.to_param}, "/+#{@group.to_param}")
     end
   end
-  
-  context "GET index" do
-    should "it is successfull" do
+
+  context "index" do
+    should "GET successfully" do
       get :index
       assert_response :success
     end
   end
-  
-  context "GET show" do
-    should "finds the requested group" do
+
+  context "show" do
+    should "find the requested group" do
       get :show, :id => @group.to_param
       assert_response :success
       assert_equal @group, assigns(:group)
     end
   end
-  
-  context 'GET to edit' do
+
+  context "edit" do
     setup do
       login_as :mike
       get :edit, :id => @group.to_param
     end
+
     should_assign_to :group
     should_respond_with :success
   end
-  
-  context 'PUT do update' do
-    should 'require user to be admin of group' do
+
+  context "update" do
+    should "require user to be admin of group" do
       put :update, :id => @group.to_param, :group => {:description => "Unskilled and unprofessional"}
-      assert_redirected_to :controller => 'sessions', :action => 'new'
+      assert_redirected_to :controller => "sessions", :action => "new"
     end
-    
-    should 'only update the description, not the name' do
+
+    should "only update the description, not the name" do
       login_as :mike
-      put :update, :id => @group.to_param, :group => {:name => 'hackers'}
-      assert_redirected_to :action => 'show'
-      assert_equal('team-thunderbird', @group.name)
+      put :update, :id => @group.to_param, :group => {:name => "hackers"}
+      assert_redirected_to :action => "show"
+      assert_equal("team-thunderbird", @group.name)
     end
-    
-    should 'update successfully' do
+
+    should "update successfully" do
       login_as :mike
-      new_description = 'We save lives'
+      new_description = "We save lives"
       put :update, :id => @group.to_param, :group => {:description => new_description}
-      assert_redirected_to :action => 'show'
+      assert_redirected_to :action => "show"
       assert_equal(new_description, @group.reload.description)
     end
   end
-  
+
   context "creating a group" do
-    should "requires login" do
+    should "require login" do
       get :new
       assert_redirected_to (new_sessions_path)
     end
-    
-    should "GETs new successfully" do
+
+    should "GET new successfully" do
       login_as :mike
       get :new
       assert_response :success
     end
-    
-    should "POST create creates a new group" do
+
+    should "POST to create a new group" do
       login_as :mike
+
       assert_difference("Group.count") do
-        post :create, :group => {:name => "foo-hackers", :description => 'Hacking the foos for your bars'},
+        post :create, :group => {:name => "foo-hackers", :description => "Hacking the foos for your bars"},
           :project => {:slug => projects(:johans).slug}
       end
+
       assert_not_equal nil, flash[:success]
-      assert !assigns(:group).new_record?, 'assigns(:group).new_record? should be false'
+      assert !assigns(:group).new_record?, "assigns(:group).new_record? should be false"
       assert_equal "foo-hackers", assigns(:group).name
       assert_equal [users(:mike)], assigns(:group).members
     end
   end
-  
+
   context "deleting a group" do
     setup do
       @group = groups(:team_thunderbird)
       @user = users(:mike)
       assert @group.admin?(@user)
     end
-    
-    should "be deletable if there is only one member" do
+
+    should "succeed if there is only one member" do
       assert_equal 1, @group.members.count
       login_as :mike
       @group.projects.destroy_all
-    
+
       assert_difference("Group.count", -1) do
         delete :destroy, :id => @group.to_param
         assert_response :redirect
@@ -134,8 +137,8 @@ class GroupsControllerTest < ActionController::TestCase
       assert_redirected_to groups_path
       assert_match(/team was deleted/, flash[:success])
     end
-    
-    should "not be deletable if there is more than one member" do
+
+    should "fail if there is more than one member" do
       @group.add_member(users(:johan), Role.member)
       assert_equal 2, @group.members.count
       login_as :mike
@@ -146,12 +149,12 @@ class GroupsControllerTest < ActionController::TestCase
       assert_redirected_to group_path(@group)
       assert_match(/team cannot be deleted/, flash[:error])
     end
-    
-    should "be deletable if there is more than one member and user is site_admin" do
+
+    should "succeed if there is more than one member and user is site_admin" do
       assert users(:johan).site_admin?
       @group.add_member(users(:johan), Role.member)
       assert_equal 2, @group.members.count
-      
+
       login_as :johan
       assert_difference("Group.count", -1) do
         delete :destroy, :id => @group.to_param
@@ -160,8 +163,8 @@ class GroupsControllerTest < ActionController::TestCase
       assert_redirected_to groups_path
       assert_match(/team was deleted/, flash[:success])
     end
-    
-    should 'be able to delete the team avatar' do
+
+    should "successfully remove the team avatar" do
       login_as :mike
       @group.update_attribute(:avatar_file_name, "foo.png")
       assert @group.avatar?
