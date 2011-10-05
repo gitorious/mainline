3dd8ddd6b866c1ec70dae70363ce0c5fd39d54e2
Christian Johansen <christian@gitorious.org>
2011-10-05 11:13:37 +0200

    Kill trailing white-space

--- app/models/project.rb
+++ app/models/project.rb
@@ -59,7 +59,7 @@ class Project < ActiveRecord::Base
     s.index "user#login", :as => "user"
     s.collect(:name, :from => "ActsAsTaggableOn::Tag", :as => "category",
       :using => "LEFT OUTER JOIN taggings ON taggings.taggable_id = projects.id " +
-      "AND taggings.taggable_type = 'Project' LEFT OUTER JOIN tags ON taggings.tag_id = tags.id")    
+      "AND taggings.taggable_type = 'Project' LEFT OUTER JOIN tags ON taggings.tag_id = tags.id")
   end
 
   NAME_FORMAT = /[a-z0-9_\-]+/.freeze
