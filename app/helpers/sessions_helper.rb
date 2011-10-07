760156703767cfe233d8746fcbdaec7f8ad71136
Christian Johansen <christian@gitorious.org>
2011-10-07 22:29:44 +0200

    Clean up white-space and formatting

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,29 +34,25 @@ module SessionsHelper
 
   def switch_login(title, action)
     link_to_function(title, <<-EOS)
-    
+
       $(".foo1").click(
       function() {
         $("body").css("background", "red");
         $("#regular_login_fields").addClass("login_hidden");
         $("#openid_login_fields").removeClass("login_hidden");
       });
-      
+
 EOS
   end
 
-
-    def switch_op_login(title, action)
-      link_to_function(title, <<-EOS)
-
-        $(".regular-switch a").click(
-        function() {
-          $("p.regular-switch").toggle();
-          $("#openid_login_fields").addClass("login_hidden");
-          $("#regular_login_fields").removeClass("login_hidden");
-        });
-
-  EOS
-    end
-    
+  def switch_op_login(title, action)
+    link_to_function(title, <<-EOS)
+      $(".regular-switch a").click(
+      function() {
+        $("p.regular-switch").toggle();
+        $("#openid_login_fields").addClass("login_hidden");
+        $("#regular_login_fields").removeClass("login_hidden");
+      });
+    EOS
+  end
 end
