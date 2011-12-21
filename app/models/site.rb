023d178d8e2b9598001b3778d780de26b0ddcac2
Christian Johansen <christian@gitorious.org>
2011-12-21 10:57:27 +0100

    Use configured site name as default site name

--- app/models/site.rb
+++ app/models/site.rb
@@ -17,14 +17,14 @@
 #++
 class Site < ActiveRecord::Base
   has_many :projects
-  
+
   validates_presence_of :title
   HTTP_CLONING_SUBDOMAIN = 'git'
   validates_exclusion_of :subdomain, :in => [HTTP_CLONING_SUBDOMAIN]
-  
+
   attr_protected :subdomain
-  
+
   def self.default
-    new(:title => "Gitorious", :subdomain => nil)
+    new(:title => GitoriousConfig["site_name"], :subdomain => nil)
   end
 end
