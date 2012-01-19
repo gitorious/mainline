35e00f018a88e3521cf7f052b1058fd68a685fc0
Marius Mathiesen <marius@gitorious.org>
2012-01-19 11:01:33 +0100

    Some gratitude is due.

--- lib/gitorious/git_shell.rb
+++ lib/gitorious/git_shell.rb
@@ -14,6 +14,9 @@
 #
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   Thanks for valuable input from joernchen of Phenoelit 
+#
 #++
 require "timeout"
 module Gitorious
