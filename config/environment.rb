39a625ebda24072226cebcd6a6e44e4f3f623096
Marius Mathiesen <marius@gitorious.org>
2012-05-29 15:00:17 +0200

    Allow configuring which Team implementation to use
    Added the configuration to environment.rb, seems to avoid some
    of the missing constant issues we've seen before.

--- config/environment.rb
+++ config/environment.rb
@@ -88,5 +88,6 @@ Rails::Initializer.run do |config|
     OAuth::Consumer::CA_FILE = nil
     Gitorious::Plugin::post_load
     Grit::Git.git_binary = GitoriousConfig["git_binary"]
+    Team.group_implementation = GitoriousConfig["group_implementation"].constantize
   end
 end
