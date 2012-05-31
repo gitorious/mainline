16e8e245ea0cb2c8fb0995ff0c826ae4f1863ddf
Marius Mathiesen <marius@gitorious.org>
2012-05-31 15:13:42 +0200

    Match LDAP-provided membership DNs to (internal) LdapGroups
    LDAP returns a list of groups a user is member of while Gitorious matches these DNs
    to those LdapGroup objects registered with these DNs set up

--- test/unit/ldap_group_test.rb
+++ test/unit/ldap_group_test.rb
@@ -1,20 +1,62 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2012 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
 require 'test_helper'
 
 class LdapGroupTest < ActiveSupport::TestCase
   context "Ldap group serialization" do
     setup {
       @group = ldap_groups(:first_ldap_group)
-      @group.member_dns = ["cn=admins","cn=developers"]
+      @group.member_dns = ["cn=testers","cn=developers"]
       @group.save
     }
 
     should "provide one membering DN per line" do
-      assert_equal("cn=admins\ncn=developers", @group.ldap_group_names)
+      assert_equal("cn=testers\ncn=developers", @group.ldap_group_names)
     end
 
     should "accept a newline separated list of member DNs" do
       @group.ldap_group_names = "cn=admin\ncn=developers"
       assert_equal(["cn=admin","cn=developers"], @group.member_dns)
     end
+  end
+
+  context "Membership" do
+    setup {
+      @group = ldap_groups(:first_ldap_group)
+      @user = users(:johan)
+    }
+
+    should "list filter LDAP groups which are known to us" do
+      stub_ldap_groups(["cn=managers", "cn=admins","cn=developers"]) do        
+        assert_equal([@group], LdapGroup.groups_for_user(@user))
+      end
+    end
+
+    should "return an empty list if no matches are found" do
+      stub_ldap_groups(["cn=managers","cn=temps"]) do
+        assert_equal([], LdapGroup.groups_for_user(@user))
+      end
+    end
+
+  end
+
+  def stub_ldap_groups(groups)
+    LdapGroup.stubs(:ldap_group_names_for_user).returns(groups)
+    yield
   end
 end
