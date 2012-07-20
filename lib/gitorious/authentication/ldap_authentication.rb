c6a386de13565ab74ef814c5bc9274e063a0fd90
Marius Mathiesen <marius@gitorious.org>
2012-07-20 14:30:17 +0200

    Make username transformation shareable
    Also some minor things:
    - don't throw an error when viewing a user without LDAP group memberships
    - don't debug LDAP output to stdout

--- lib/gitorious/authentication/ldap_authentication.rb
+++ lib/gitorious/authentication/ldap_authentication.rb
@@ -113,7 +113,7 @@ module Gitorious
       # Transform a username usable towards LDAP into something that passes Gitorious'
       # username validations
       def transform_username(username)
-        username.gsub(".", "-")
+        LDAPConfigurator.transform_username(username)
       end
 
       def auto_register(username)
