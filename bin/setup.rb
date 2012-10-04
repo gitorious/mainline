2726e059e62d311a7ae1ba51fa0d5977b6d3a586
Marius Mathiesen <marius@gitorious.org>
2012-10-04 13:03:12 +0200

    Introducing $GITORIOUS_ROOT/bin for CLI work with Gitorious
    In order to make it easier to run Gitorious CLI tools directly,
    without dealing with switching users, running `bundle exec` and so,
    we're now shipping some preconfigured scripts inside bin/.
    
    These scripts will load a functional Gitorious environment and (if
    running in production mode) require that either the user configured in
    gitorious.yml is used or switch to that user before running the
    scripts. This means these scripts can be run either as the configured
    user (usually "git") or by root (in which case the scripts will switch
    to that user.
    
    The first two CLI tools included are rake (which lets you invoke Rake)
    and unicorn (which lets you run Unicorn).

---
+++ bin/setup.rb
@@ -0,0 +1,58 @@
+# Wrapper script for Gitorious-related CLIs
+# It will :
+# - set up an environment for bundler
+# - check if a specific user is required in gitorious.yml
+# - if so: check if we are it
+# - if not and we are root: setuid+setgid to that user
+# - if not and we are not root, fail
+
+module Gitorious
+  class CLI
+    def run_with_gitorious_environment
+      setup_environment
+      require_valid_user!
+      yield
+    end
+
+    def setup_environment
+      require "pathname"
+      require "rubygems"
+      ENV["BUNDLE_GEMFILE"] = (Pathname(rails_root) + "Gemfile").to_s
+      ENV["RAILS_ENV"] ||= "production"
+      require "bundler/setup"
+    end
+
+    def rails_root
+      rails_root ||= (Pathname(__FILE__) + "../../").realpath.to_s
+    end
+
+    def require_valid_user!
+      if rails_env == "production"
+        if git_user = gitorious_config("gitorious_user")
+          etc_user = Etc.getpwnam(git_user)
+          uid = etc_user.uid
+          gid = etc_user.gid
+          current_userid = Process.euid
+          if current_userid == uid
+            # OK, running as correct user
+          else
+            if current_userid == 0
+              Process::GID.change_privilege(gid)
+              Process::UID.change_privilege(uid)
+            else
+              raise "You need to be root to do this!"
+            end
+          end
+        end
+      end
+    end
+
+    def gitorious_config(key)
+      YAML::load_file(rails_root + "/config/gitorious.yml")[rails_env][key]
+    end
+
+    def rails_env
+      ENV["RAILS_ENV"]
+    end
+  end
+end
