edaebc8129c851b574573041a1e6817c61c1f0b2
Peter Kjellerstedt <pkj@axis.com>
2012-10-10 12:19:26 +0200

    Correct an old migration
    I do not really know why, but without this modified version of this
    migration (which should do exactly the same as the original) I got:
    
    Mysql::Error: Table 'gitorious_production.content_memberships' doesn't
    exist: SELECT * FROM `content_memberships`
    
    when I tried to migrate from v2.1.1 to v2.3.1.

--- db/migrate/20120223093906_make_project_membership_polymorphic_on_subject.rb
+++ db/migrate/20120223093906_make_project_membership_polymorphic_on_subject.rb
@@ -1,28 +1,28 @@
-class ProjectMembership < ActiveRecord::Base
+class ContentMembership < ActiveRecord::Base
   belongs_to :project
   belongs_to :member, :polymorphic => true
 end
 
 class MakeProjectMembershipPolymorphicOnSubject < ActiveRecord::Migration
   def self.up
-    add_column :project_memberships, :content_type, :string
-    ProjectMembership.all.each do |pm|
-      pm.content_type = "Project"
-      pm.save!
-    end
     rename_table :project_memberships, :content_memberships
     rename_column :content_memberships, :project_id, :content_id
+    add_column :content_memberships, :content_type, :string
+    ContentMembership.all.each do |cm|
+      cm.content_type = "Project"
+      cm.save!
+    end
   end
 
   # Destructive - removes any non-project memberships
   def self.down
-    rename_table :content_memberships, :project_memberships
-
-    ProjectMembership.all.each do |pm|
-      pm.delete if pm.content_type != "Project"
+    ContentMembership.all.each do |cm|
+      cm.delete if cm.content_type != "Project"
     end
 
-    remove_column :project_memberships, :content_type
-    rename_column :project_memberships, :content_id, :project_id
+    remove_column :content_memberships, :content_type
+    rename_column :content_memberships, :content_id, :project_id
+
+    rename_table :content_memberships, :project_memberships
   end
 end
