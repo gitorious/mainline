55502d661d4c1c8236e95a634162587bfb605837
Christian Johansen <christian@gitorious.com>
2012-10-26 13:02:38 +0200

    Temporary routing setup for Rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,93 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2012 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
 Gitorious::Application.routes.draw do
   root :to => 'site#index'
+
   match 'users/activate/:activation_code' => 'users#activate'
   match 'users/pending_activation' => 'users#pending_activation'
   match 'users/reset_password/:token' => 'users#reset_password', :as => :reset_password
+
+  repository_routing = Proc.new do
+    member do
+      post :create_clone
+      get :clone
+      get :search_clones
+      get :config
+      get :committers
+      get :confirm_delete
+      get :writable_by
+    end
+
+    resources :comments
+
+    match 'comments/commit/:sha' => 'comments#commit', :as => :commit_comment, :via => :get
+    match 'comments/preview' => 'comments#preview', :as => :comments_preview
+
+    resources :merge_requests do
+      collection do
+        post :create
+        post :commit_list
+        post :target_branches
+      end
+
+      member do
+        get :commit_status
+        get :version
+        get :terms_accepted
+      end
+
+      resources :comments do
+        collection do
+          post :preview
+        end
+      end
+
+      resources :merge_request_versions do
+        resources :comments do
+          collection do
+            post :preview
+          end
+        end
+      end
+    end
+
+    resources :repository_memberships
+    resources :committerships
+
+    match 'commits/*branch/feed.:format' => 'commits#feed', :as => :formatted_commits_feed#, :via =>
+    match 'commits' => 'commits#index', :as => :commits
+    match 'commits/*branch' => 'commits#index', :as => :commits_in_ref
+    match 'graph' => 'graphs#index', :as => :graph
+    match 'graph/*branch' => 'graphs#index', :as => :graph_in_ref
+    match 'commit/:id/comments' => 'commit_comments#index', :as => :commit_comments, :id => /[^\/]+/
+    match 'commit/:id/diffs' => 'commit_diffs#index', :as => :commit_diffs, :id => /[^\/]+/
+    match 'commit/:from_id/diffs/:id' => 'commit_diffs#compare', :as => :commit_compare
+    match 'commit/:id' => 'commits#show', :as => :commit, :id => /.*/
+    match 'trees/' => 'trees#index', :as => :trees
+    match 'trees/*branch_and_path' => 'trees#show', :as => :tree
+    match 'trees/*branch_and_path.:format' => 'trees#show', :as => :formatted_tree
+    match 'archive-tarball/*branch' => 'trees#archive', :as => :archive_tar, :archive_format => 'tar.gz'
+    match 'blobs/raw/*branch_and_path' => 'blobs#raw', :as => :raw_blob
+    match 'blobs/history/*branch_and_path' => 'blobs#history', :as => :blob_history
+    match 'blobs/blame/*branch_and_path' => 'blobs#blame', :as => :blame
+    match 'blobs/*branch_and_path' => 'blobs#show', :as => :blob
+  end
 
   resources :users do
     collection do
@@ -31,138 +116,10 @@ Gitorious::Application.routes.draw do
 
     resource :license
 
-    resources :repositories do
-      member do
-        post :create_clone
-        get :clone
-        get :search_clones
-        get :config
-        get :committers
-        get :confirm_delete
-        get :writable_by
-      end
-
-      resources :comments
-
-      match 'comments/commit/:sha' => 'comments#commit', :as => :commit_comment, :via => :get
-      match 'comments/preview' => 'comments#preview', :as => :comments_preview
-
-      resources :merge_requests do
-        collection do
-          post :create
-          post :commit_list
-          post :target_branches
-        end
-
-        member do
-          get :commit_status
-          get :version
-          get :terms_accepted
-        end
-
-        resources :comments do
-          collection do
-            post :preview
-          end
-        end
-
-        resources :merge_request_versions do
-          resources :comments do
-            collection do
-              post :preview
-            end
-          end
-        end
-      end
-
-      resources :repository_memberships
-      resources :committerships
-
-      match 'commits/*branch/feed.:format' => 'commits#feed', :as => :formatted_commits_feed#, :via =>
-      match 'commits' => 'commits#index', :as => :commits
-      match 'commits/*branch' => 'commits#index', :as => :commits_in_ref
-      match 'graph' => 'graphs#index', :as => :graph
-      match 'graph/*branch' => 'graphs#index', :as => :graph_in_ref
-      match 'commit/:id/comments' => 'commit_comments#index', :as => :commit_comments, :id => /[^\/]+/
-      match 'commit/:id/diffs' => 'commit_diffs#index', :as => :commit_diffs, :id => /[^\/]+/
-      match 'commit/:from_id/diffs/:id' => 'commit_diffs#compare', :as => :commit_compare
-      match 'commit/:id' => 'commits#show', :as => :commit, :id => /.*/
-      match 'trees/' => 'trees#index', :as => :trees
-      match 'trees/*branch_and_path' => 'trees#show', :as => :tree
-      match 'trees/*branch_and_path.:format' => 'trees#show', :as => :formatted_tree
-      match 'archive-tarball/*branch' => 'trees#archive', :as => :archive_tar, :archive_format => 'tar.gz'
-      match 'blobs/raw/*branch_and_path' => 'blobs#raw', :as => :raw_blob
-      match 'blobs/history/*branch_and_path' => 'blobs#history', :as => :blob_history
-      match 'blobs/blame/*branch_and_path' => 'blobs#blame', :as => :blame
-      match 'blobs/*branch_and_path' => 'blobs#show', :as => :blob
-    end
+    resources :repositories, &repository_routing
 
     resources :projects do
-      resources :repositories do
-        member do
-          post :create_clone
-          get :clone
-          get :search_clones
-          get :config
-          get :committers
-          get :confirm_delete
-          get :writable_by
-        end
-
-        resources :comments
-
-        match 'comments/commit/:sha' => 'comments#commit', :as => :commit_comment, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }, :via => :get
-        match 'comments/preview' => 'comments#preview', :as => :comments_preview, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-
-        resources :merge_requests do
-          collection do
-            post :create
-            post :commit_list
-            post :target_branches
-          end
-
-          member do
-            get :commit_status
-            get :version
-            get :terms_accepted
-          end
-
-          resources :comments do
-            collection do
-              post :preview
-            end
-          end
-
-          resources :merge_request_versions do
-            resources :comments do
-              collection do
-                post :preview
-              end
-            end
-          end
-        end
-
-        resources :repository_memberships
-        resources :committerships
-
-        match 'commits/*branch/feed.:format' => 'commits#feed', :as => :formatted_commits_feed, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }#, :via =>
-        match 'commits' => 'commits#index', :as => :commits, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'commits/*branch' => 'commits#index', :as => :commits_in_ref, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'graph' => 'graphs#index', :as => :graph, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'graph/*branch' => 'graphs#index', :as => :graph_in_ref, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'commit/:id/comments' => 'commit_comments#index', :as => :commit_comments, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }, :id => /[^\/]+/
-        match 'commit/:id/diffs' => 'commit_diffs#index', :as => :commit_diffs, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }, :id => /[^\/]+/
-        match 'commit/:from_id/diffs/:id' => 'commit_diffs#compare', :as => :commit_compare, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'commit/:id' => 'commits#show', :as => :commit, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }, :id => /.*/
-        match 'trees/' => 'trees#index', :as => :trees, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'trees/*branch_and_path' => 'trees#show', :as => :tree, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'trees/*branch_and_path.:format' => 'trees#show', :as => :formatted_tree, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'archive-tarball/*branch' => 'trees#archive', :as => :archive_tar, :archive_format => 'tar.gz', :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'blobs/raw/*branch_and_path' => 'blobs#raw', :as => :raw_blob, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'blobs/history/*branch_and_path' => 'blobs#history', :as => :blob_history, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'blobs/blame/*branch_and_path' => 'blobs#blame', :as => :blame, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-        match 'blobs/*branch_and_path' => 'blobs#show', :as => :blob, :constraints => { :user_id => /(?i-mx:[a-z0-9\-_\.]+)/i }
-      end
+      resources :repositories , &repository_routing
     end
   end
 
@@ -175,7 +132,6 @@ Gitorious::Application.routes.draw do
   resources :user_auto_completions, :only => [:index]
   resources :group_auto_completions, :only => [:index]
 
-  #match '/sessions' => 'sessions#create', :as => :open_id_complete, :constraints => { :method => :get }
   match '/sessions' => 'sessions#create', :as => :open_id_complete, :via => :get
 
   resource :sessions
@@ -186,138 +142,10 @@ Gitorious::Application.routes.draw do
     end
 
     resources :memberships
-    resources :repositories do
-
-      member do
-        post :create_clone
-        get :clone
-        get :search_clones
-        get :config
-        get :committers
-        get :confirm_delete
-        get :writable_by
-      end
-
-      resources :comments
-
-      match 'comments/commit/:sha' => 'comments#commit', :as => :commit_comment, :via => :get
-      match 'comments/preview' => 'comments#preview', :as => :comments_preview
-
-      resources :merge_requests do
-        collection do
-          post :create
-          post :commit_list
-          post :target_branches
-        end
-
-        member do
-          get :commit_status
-          get :version
-          get :terms_accepted
-        end
-
-        resources :comments do
-          collection do
-            post :preview
-          end
-        end
-
-        resources :merge_request_versions do
-          resources :comments do
-            collection do
-              post :preview
-            end
-          end
-        end
-      end
-
-      resources :repository_memberships
-      resources :committerships
-      match 'commits/*branch/feed.:format' => 'commits#feed', :as => :formatted_commits_feed#, :via =>
-      match 'commits' => 'commits#index', :as => :commits
-      match 'commits/*branch' => 'commits#index', :as => :commits_in_ref
-      match 'graph' => 'graphs#index', :as => :graph
-      match 'graph/*branch' => 'graphs#index', :as => :graph_in_ref
-      match 'commit/:id/comments' => 'commit_comments#index', :as => :commit_comments, :id => /[^\/]+/
-      match 'commit/:id/diffs' => 'commit_diffs#index', :as => :commit_diffs, :id => /[^\/]+/
-      match 'commit/:from_id/diffs/:id' => 'commit_diffs#compare', :as => :commit_compare
-      match 'commit/:id' => 'commits#show', :as => :commit, :id => /.*/
-      match 'trees/' => 'trees#index', :as => :trees
-      match 'trees/*branch_and_path' => 'trees#show', :as => :tree
-      match 'trees/*branch_and_path.:format' => 'trees#show', :as => :formatted_tree
-      match 'archive-tarball/*branch' => 'trees#archive', :as => :archive_tar, :archive_format => 'tar.gz'
-      match 'blobs/raw/*branch_and_path' => 'blobs#raw', :as => :raw_blob
-      match 'blobs/history/*branch_and_path' => 'blobs#history', :as => :blob_history
-      match 'blobs/blame/*branch_and_path' => 'blobs#blame', :as => :blame
-      match 'blobs/*branch_and_path' => 'blobs#show', :as => :blob
-    end
+    resources :repositories, &repository_routing
 
     resources :projects do
-      resources :repositories do
-        member do
-          post :create_clone
-          get :clone
-          get :search_clones
-          get :config
-          get :committers
-          get :confirm_delete
-          get :writable_by
-        end
-
-        resources :comments
-
-        match 'comments/commit/:sha' => 'comments#commit', :as => :commit_comment, :via => :get
-        match 'comments/preview' => 'comments#preview', :as => :comments_preview
-
-        resources :merge_requests do
-          collection do
-            post :create
-            post :commit_list
-            post :target_branches
-          end
-
-          member do
-            get :commit_status
-            get :version
-            get :terms_accepted
-          end
-
-          resources :comments do
-            collection do
-              post :preview
-            end
-          end
-
-          resources :merge_request_versions do
-            resources :comments do
-              collection do
-                post :preview
-              end
-            end
-          end
-        end
-
-        resources :repository_memberships
-        resources :committerships
-
-        match 'commits/*branch/feed.:format' => 'commits#feed', :as => :formatted_commits_feed#, :via =>
-        match 'commits' => 'commits#index', :as => :commits
-        match 'commits/*branch' => 'commits#index', :as => :commits_in_ref
-        match 'graph' => 'graphs#index', :as => :graph
-        match 'graph/*branch' => 'graphs#index', :as => :graph_in_ref
-        match 'commit/:id/comments' => 'commit_comments#index', :as => :commit_comments, :id => /[^\/]+/
-        match 'commit/:id/diffs' => 'commit_diffs#index', :as => :commit_diffs, :id => /[^\/]+/
-        match 'commit/:from_id/diffs/:id' => 'commit_diffs#compare', :as => :commit_compare
-        match 'commit/:id' => 'commits#show', :as => :commit, :id => /.*/
-        match 'trees/' => 'trees#index', :as => :trees
-        match 'trees/*branch_and_path' => 'trees#show', :as => :tree
-        match 'trees/*branch_and_path.:format' => 'trees#show', :as => :formatted_tree
-        match 'archive-tarball/*branch' => 'trees#archive', :as => :archive_tar, :archive_format => 'tar.gz'
-        match 'blobs/raw/*branch_and_path' => 'blobs#raw', :as => :raw_blob
-        match 'blobs/history/*branch_and_path' => 'blobs#history', :as => :blob_history
-        match 'blobs/blame/*branch_and_path' => 'blobs#blame', :as => :blame
-        match 'blobs/*branch_and_path' => 'blobs#show', :as => :blob
-      end
+      resources :repositories, &repository_routing
     end
   end
 
@@ -356,70 +184,7 @@ Gitorious::Application.routes.draw do
       end
     end
 
-    resources :repositories do
-      member do
-        post :create_clone
-        get :clone
-        get :search_clones
-        get :config
-        get :committers
-        get :confirm_delete
-        get :writable_by
-      end
-
-      resources :comments
-
-      match 'comments/commit/:sha' => 'comments#commit', :as => :commit_comment, :via => :get
-      match 'comments/preview' => 'comments#preview', :as => :comments_preview
-
-      resources :merge_requests do
-        collection do
-          post :create
-          post :commit_list
-          post :target_branches
-        end
-
-        member do
-          get :commit_status
-          get :version
-          get :terms_accepted
-        end
-
-        resources :comments do
-          collection do
-            post :preview
-          end
-        end
-
-        resources :merge_request_versions do
-          resources :comments do
-            collection do
-              post :preview
-            end
-          end
-        end
-      end
-
-      resources :repository_memberships
-      resources :committerships
-      match 'commits/*branch/feed.:format' => 'commits#feed', :as => :formatted_commits_feed#, :via =>
-      match 'commits' => 'commits#index', :as => :commits
-      match 'commits/*branch' => 'commits#index', :as => :commits_in_ref
-      match 'graph' => 'graphs#index', :as => :graph
-      match 'graph/*branch' => 'graphs#index', :as => :graph_in_ref
-      match 'commit/:id/comments' => 'commit_comments#index', :as => :commit_comments, :id => /[^\/]+/
-      match 'commit/:id/diffs' => 'commit_diffs#index', :as => :commit_diffs, :id => /[^\/]+/
-      match 'commit/:from_id/diffs/:id' => 'commit_diffs#compare', :as => :commit_compare
-      match 'commit/:id' => 'commits#show', :as => :commit, :id => /.*/
-      match 'trees/' => 'trees#index', :as => :trees
-      match 'trees/*branch_and_path' => 'trees#show', :as => :tree
-      match 'trees/*branch_and_path.:format' => 'trees#show', :as => :formatted_tree
-      match 'archive-tarball/*branch' => 'trees#archive', :as => :archive_tar, :archive_format => 'tar.gz'
-      match 'blobs/raw/*branch_and_path' => 'blobs#raw', :as => :raw_blob
-      match 'blobs/history/*branch_and_path' => 'blobs#history', :as => :blob_history
-      match 'blobs/blame/*branch_and_path' => 'blobs#blame', :as => :blame
-      match 'blobs/*branch_and_path' => 'blobs#show', :as => :blob
-    end
+    resources :repositories, &repository_routing
   end
 
   resource :search
@@ -480,5 +245,5 @@ Gitorious::Application.routes.draw do
   match '/activities' => 'site#public_timeline', :as => :activity
   match '/merge_request_landing_page' => 'merge_requests#oauth_return', :as => :merge_request_landing_page
   match '/merge_requests/:id' => 'merge_requests#direct_access', :as => :merge_request_direct_access
-  match '/:controller(/:action(/:id))'
+  # match '/:controller(/:action(/:id))'
 end
