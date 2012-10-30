bf9b8a6a03b3c73da7662fc9b1a4dfca868be706
Christian Johansen <christian@gitorious.com>
2012-10-30 09:11:43 +0100

    Rails wants invalid ERB

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -100,7 +100,7 @@ module AuthenticatedSystem
     # Store the URI of the current request in the session.
     #
     # We can return to this location by calling #redirect_back_or_default.
-    def store_location(location = request.request_uri)
+    def store_location(location = request.fullpath)
       session[:return_to] = location
     end
 
