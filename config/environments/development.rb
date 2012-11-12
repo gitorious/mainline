19efeb420c3008becfccfccec5a9c052b18633ad
Christian Johansen <christian@gitorious.com>
2012-11-12 15:36:27 +0100

    Remove invalid comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,4 @@ Gitorious::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # TODO: UPGRADE
-  #
-  # ExceptionNotifier.exception_recipients = gts_config["exception_notification_emails"]
 end
