82cd5b545572c82600eed368099e8057879ad7e4
Marius Mathiesen <marius@gitorious.org>
2012-11-21 13:59:51 +0100

    Read host and port from config when checking if site is up

--- lib/gitorious/diagnostics.rb
+++ lib/gitorious/diagnostics.rb
@@ -108,7 +108,8 @@ module Gitorious
     end
 
     def web_interface_reachable?
-      `curl --silent localhost:80 | grep Gitorious | wc -l`.to_i > 0
+      url = Gitorious.site.url("/")
+      `curl --silent #{url} | grep Gitorious | wc -l`.to_i > 0
     end
 
     def git_user_ok?
