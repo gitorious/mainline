8780dbd2f94ff32375da45366e3eea82ef9d0596
Christian Johansen <christian@gitorious.com>
2012-11-21 10:26:40 +0100

    Add big fat warning if attempting to run Gitorious on wrong Ruby version

---
+++ config/initializers/ruby_version.rb
@@ -0,0 +1,18 @@
+if !%w[1.8.7 1.9.3].include?(RUBY_VERSION)
+  $stderr.puts <<-WARN
+######################################################################
+#                              WARNING!                              #
+######################################################################
+#                                                                    #
+# Your Ruby version, #{RUBY_VERSION} may not be supported. Gitorious #
+# has only been tested with Ruby 1.8.7 and 1.9.3. Please consider    #
+# using one of these officially supported versions. If you feel that #
+# this warning has been issued in error, please let us know at       #
+# http://groups.google.com/group/gitorious/                          #
+#                                                                    #
+# Please note that Ruby 1.9.1 and 1.9.2 should not be used. Upgrade  #
+# to 1.9.3.                                                          #
+#                                                                    #
+######################################################################
+  WARN
+end
