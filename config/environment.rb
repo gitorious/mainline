ded49bbc1b9922f4f89343baec1829b5439d4d87
Marius Mathiesen <marius@gitorious.org>
2013-01-09 08:53:01 +0100

    Gitorious 2.4.5
    This release fixes the vulnerability CVE-2013-0156.

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@
 # you don't control web/app server and can't set it the proper way
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
