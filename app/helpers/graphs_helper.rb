c914a4df2947720ae45c3f22aad4e51dad7f6ef6
Christian Johansen <christian@gitorious.com>
2013-02-12 23:12:36 +0100

    Include a "spacer" to avoid JS concatenation bugs

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -22,6 +22,6 @@ module GraphsHelper
   def capillary_js_paths
     formatters = %w[scale svg-data raphael message-markup].collect { |p| "formatters/#{p}" }
     paths = (%w[branch graph] + formatters).collect { |p| "lib/capillary/lib/capillary/#{p}" }
-    ["lib/raphael/raphael-min.js", "lib/bane/lib/bane.js", "lib/capillary/lib/capillary"] + paths
+    ["lib/raphael/raphael-min.js", "spacer.js", "lib/bane/lib/bane.js", "lib/capillary/lib/capillary"] + paths
   end
 end
---
+++ public/javascripts/spacer.js
@@ -0,0 +1,5 @@
+/**
+ * This is kinda sad, but sometimes needed in between scripts that do
+ * not work when concatenated.
+ */
+;
