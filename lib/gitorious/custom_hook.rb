c341ed629122714601cf5daa652b582a77f84ed3
Christian Johansen <christian@gitorious.com>
2013-03-05 22:32:33 +0100

    Bug: Custom hooks when GitoriousConfig is loaded
    If GitoriousConfig was already loaded when the hooks fired, then
    CustomHook would attempt to access GitoriousConfig[env] which is
    senseless, because GitoriousConfig only contains configuration for a
    given environment.

--- lib/gitorious/custom_hook.rb
+++ lib/gitorious/custom_hook.rb
@@ -30,8 +30,7 @@ module Gitorious
       root = File.expand_path(File.join(File.dirname(__FILE__), "../../"))
       @file = File.join(root, "data/hooks/custom-#{@hook}")
       return File.expand_path(@file) if File.executable?(@file)
-      config = defined?(GitoriousConfig) ? GitoriousConfig : YAML::load_file(root + "/config/gitorious.yml")
-      config = config[@env] || {}
+      config = defined?(GitoriousConfig) ? GitoriousConfig : YAML::load_file(root + "/config/gitorious.yml")[@env]
       @file = config["custom_#{@hook.gsub(/\-/, '_')}_hook"]
     end
 
