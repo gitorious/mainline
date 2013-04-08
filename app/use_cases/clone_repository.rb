2c79802b31b9ba6c316573dc51e79d6d96560ac0
Christian Johansen <christian@gitorious.com>
2013-04-08 11:37:23 +0200

    Make sure use cases loads commands/input class

--- app/use_cases/clone_repository.rb
+++ app/use_cases/clone_repository.rb
@@ -16,6 +16,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
 require "use_case"
+require "commands/clone_repository_command"
 
 class CloneRepository
   include UseCase
