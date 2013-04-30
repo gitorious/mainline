13031f453f6b9ea22b1f5faffaa042515e2a63c9
Christian Johansen <christian@gitorious.com>
2013-04-30 11:30:17 +0200

    Add generate password reset token use case

---
+++ app/use_cases/generate_password_reset_token.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+require "use_case"
+require "virtus"
+
+class GeneratePasswordResetTokenCommand
+  def initialize(user)
+    @user = user
+  end
+
+  def execute(user)
+    user.password_key = self.class.generate_reset_key
+    user.save!
+    Mailer.forgotten_password(user, user.password_key).deliver
+    user
+  end
+
+  def build(params)
+    @user
+  end
+
+  def self.generate_reset_key(n = 16)
+    SecureRandom.hex(n)
+  end
+end
+
+GeneratePasswordResetTokenValidator = UseCase::Validator.define do
+  validate :user_activated
+
+  def user_activated
+    return if activated?
+    message = I18n.t("users_controller.reset_password_inactive_account")
+    errors.add(:activated?, message)
+  end
+end
+
+class GeneratePasswordResetToken
+  include UseCase
+
+  def initialize(user)
+    add_pre_condition(UserRequired.new(user))
+    step(GeneratePasswordResetTokenCommand.new(user), :validator => GeneratePasswordResetTokenValidator)
+  end
+end
---
+++ test/unit/use_cases/generate_password_reset_token_test.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+require "test_helper"
+require "generate_password_reset_token"
+
+class GeneratePasswordResetTokenTest < ActiveSupport::TestCase
+  should "fail without user" do
+    outcome = GeneratePasswordResetToken.new(nil).execute
+
+    refute outcome.success?, outcome.to_s
+    assert outcome.pre_condition_failed?, outcome.to_s
+  end
+
+  should "fail with inactive user" do
+    user = users(:zmalltalker)
+    user.activation_code = "123456"
+    user.save
+
+    outcome = GeneratePasswordResetToken.new(user).execute
+
+    refute outcome.success?, outcome.to_s
+    refute outcome.pre_condition_failed?, outcome.to_s
+  end
+
+  should "generate password key and email it to the user" do
+    outcome = GeneratePasswordResetToken.new(users(:zmalltalker)).execute
+
+    refute_nil users(:zmalltalker).reload.password_key
+    assert_equal 1, ActionMailer::Base.deliveries.length
+    pattern = /reset your password\: http\:\/\/gitorious.test\/users\/reset_password\/[0-9a-f]+/
+    assert_match pattern, ActionMailer::Base.deliveries.last.body.to_s
+  end
+end
