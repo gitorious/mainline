c776d83abbf0e2edc9c679f83823c422ca6773cb
Christian Johansen <christian@gitorious.com>
2013-06-27 09:50:47 +0200

    No layout with the new 404 file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -174,7 +174,7 @@ class ApplicationController < ActionController::Base
   def render_not_found
     render({ :file => Rails.root + "public/404.html",
              :status => 404,
-             :layout => "application" })
+             :layout => false })
   end
 
   def render_git_timeout
