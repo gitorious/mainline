6f156209f4898be5e794b5a8cd236f7443f771a0
Ken Dreyer <ktdreyer@ktdreyer.com>
2013-06-27 17:22:59 -0600

    don't warn with Ruby 2.0.0
    The test suite passes with Ruby 2.0.0, so add that to the list of
    "supported" Ruby versions.

--- config/initializers/ruby_version.rb
+++ config/initializers/ruby_version.rb
@@ -1,17 +1,17 @@
-if !%w[1.8.7 1.9.3].include?(RUBY_VERSION)
+if !%w[1.8.7 1.9.3 2.0.0].include?(RUBY_VERSION)
   $stderr.puts <<-WARN
 ######################################################################
 #                              WARNING!                              #
 ######################################################################
 #                                                                    #
 # Your Ruby version, #{RUBY_VERSION} may not be supported. Gitorious #
-# has only been tested with Ruby 1.8.7 and 1.9.3. Please consider    #
-# using one of these officially supported versions. If you feel that #
-# this warning has been issued in error, please let us know at       #
-# http://groups.google.com/group/gitorious/                          #
+# has only been tested with Ruby 1.8.7, 1.9.3, and 2.0.0. Please     #
+# consider using one of these officially supported versions. If you  #
+# feel that this warning has been issued in error, please let us     #
+# know at http://groups.google.com/group/gitorious/                  #
 #                                                                    #
 # Please note that Ruby 1.9.1 and 1.9.2 should not be used. Upgrade  #
-# to 1.9.3.                                                          #
+# to 1.9.3 or 2.0.0                                                  #
 #                                                                    #
 ######################################################################
   WARN
