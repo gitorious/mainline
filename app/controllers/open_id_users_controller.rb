a87b12433d4b160b1b361d759330838423905fb3
Christian Johansen <christian@gitorious.com>
2013-07-03 09:42:08 +0200

    Fix missing require

--- app/controllers/open_id_users_controller.rb
+++ app/controllers/open_id_users_controller.rb
@@ -15,6 +15,7 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
+require "use_cases/create_openid_user"
 
 class OpenIdUsersController < ApplicationController
   renders_in_global_context
