2952b46302b1f6315b6e603b2003d97ef4b74022
Christian Johansen <christian@gitorious.com>
2013-07-11 22:59:34 +0200

    Add CreateWebHooks use case

---
+++ app/use_cases/create_web_hook.rb
@@ -0,0 +1,60 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+require "use_case"
+require "virtus"
+require "validators/web_hook_validator"
+
+class WebHookParams
+  include Virtus
+  attribute :url, String
+  attribute :site_wide, Boolean
+end
+
+class CreateWebHookCommand
+  def initialize(app, repository, user)
+    @app = app
+    @repository = repository
+    @user = user
+  end
+
+  def execute(web_hook)
+    web_hook.save!
+    web_hook
+  end
+
+  def build(params)
+    WebHook.new({
+        :url => params.url,
+        :repository => params.site_wide ? nil : repository,
+        :user => user
+      })
+  end
+
+  private
+  attr_reader :user, :repository
+end
+
+class CreateWebHook
+  include UseCase
+
+  def initialize(app, repository, user)
+    input_class(WebHookParams)
+    add_pre_condition(AdminRequired.new(app, repository, user))
+    step(CreateWebHookCommand.new(app, repository, user), :validator => WebHookValidator)
+  end
+end
---
+++ test/unit/use_cases/create_web_hook_test.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+require "test_helper"
+require "create_web_hook"
+
+class CreateWebHookTest < ActiveSupport::TestCase
+  should "create web hook" do
+    user = users(:johan)
+    outcome = CreateWebHook.new(Gitorious::App, Repository.first, user).execute({
+        :url => "http://example.com"
+      })
+
+    assert outcome.success?, outcome.failure.inspect
+    assert_equal Repository.first, outcome.result.repository
+    assert_equal user, outcome.result.user
+    assert_equal "http://example.com", outcome.result.url
+  end
+
+  should "fail if user is not a repository admin" do
+    user = users(:johan)
+    outcome = CreateWebHook.new(Gitorious::App, repositories(:moes), user).execute({
+        :url => "http://example.com"
+      })
+
+    refute outcome.success?
+    assert outcome.pre_condition_failed?
+  end
+
+  should "create site-wide web hook" do
+    user = users(:johan)
+    outcome = CreateWebHook.new(Gitorious::App, repositories(:johans), user).execute({
+        :url => "http://example.com",
+        :site_wide => true
+      })
+
+    assert outcome.success?
+    assert outcome.result.global?
+  end
+end
