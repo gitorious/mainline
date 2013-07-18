3aac9416ed647fe887e73e91524db20670e3e493
Christian Johansen <christian@gitorious.com>
2013-07-18 16:29:13 +0200

    Projects#edit almost there

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -23,37 +23,9 @@ module ProjectsHelper
   include RepositoriesHelper
   include Gitorious::Authorization
 
-  def show_new_project_link?
-    return logged_in? && !ProjectProposal.required?(current_user)
-  end
-
   def wiki_permission_choices
     [["Writable by everyone", WikiRepository::WRITABLE_EVERYONE],
      ["Writable by project members", WikiRepository::WRITABLE_PROJECT_MEMBERS]]
-  end
-
-  def add_status_link(form_builder)
-    form_builder.fields_for(:merge_request_statuses, MergeRequestStatus.new,
-                            :child_index => 'NEW_RECORD') do |f|
-      html = render(:partial => 'merge_request_status_form',
-                    :locals => { :form => f, :project_form => nil })
-      link_to_function("Add status", "$('#merge_request_statuses').append(" +
-                       "'#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()))" +
-                       ".find('input:last').SevenColorPicker();")
-    end
-  end
-
-  def remove_status_link(form_builder)
-    remove_img = image_tag("silk/delete.png", :title => "Remove")
-    if form_builder.object.new_record?
-      # just remove it from the DOM
-      link_to_function(remove_img, "$(this).parents('.merge_request_status').remove();");
-    else
-      # Set the proper hidden html flag to _delete
-      form_builder.hidden_field(:_destroy) +
-        link_to_function(remove_img, "$(this).parents('.merge_request_status').hide();
-          $(this).prev().val('1')")
-    end
   end
 
   def default_license(project)
