6c591020d6f2a6b4a157415b4f49a5a407a0528f
Piotr Solnica <piotr.solnica@gmail.com>
2013-08-22 13:08:36 +0200

    Add Flowdock service

---
+++ app/models/service/flowdock.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
+class Service::Flowdock < Service::Adapter
+  URL = "https://api.flowdock.com/v1/git/"
+
+  label "Flowdock"
+
+  attributes :flow_token
+
+  validates :flow_token, :presence => true
+
+  def name
+    "Flowdock"
+  end
+
+  def notify(http_client, payload)
+    http_client.post url, :body => payload.to_json, :content_type => "application/json"
+  end
+
+  private
+
+  def url
+    "#{URL}/#{flow_token}"
+  end
+end
---
+++ test/unit/service/flowdock_test.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
+require 'test_helper'
+
+class Service::FlowdockTest < ActiveSupport::TestCase
+  def flowdock(attrs = {})
+    Service::Flowdock.new({ :flow_token => 'foobarbaz' }.merge(attrs))
+  end
+
+  should "validate presence of flow_token" do
+    assert sprintly(:flow_token => "foo").valid?
+    refute sprintly(:flow_token => "").valid?
+  end
+
+  context "#notify" do
+    should "send payload to flowdock" do
+      payload = { "foo" => "bar" }
+      http = mock
+      http.expects(:post).with(
+        "#{Service::FlowdockTest::URL}/foobarbaz",
+        :body => payload.to_json,
+        :content_type => 'application/json'
+      )
+      sprintly.notify(http, payload)
+    end
+  end
+end
