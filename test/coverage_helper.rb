b2917355e31f558f546e5ddacc4d88fa41d820af
Piotr Solnica <piotr.solnica@gmail.com>
2013-08-22 11:05:06 +0200

    Update simplecov config
      * Use multiple formatters: html and rcov
      * Filter out test and vendor files

--- test/coverage_helper.rb
+++ test/coverage_helper.rb
@@ -1,6 +1,13 @@
 if ENV['COVERAGE'] && RUBY_VERSION > "1.9"
   require "simplecov"
   require "simplecov-rcov"
-  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-  SimpleCov.start("rails")
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::RcovFormatter
+  ]
+
+  SimpleCov.start("rails") do
+    add_filter '/test/'
+    add_filter '/vendor/'
+  end
 end
