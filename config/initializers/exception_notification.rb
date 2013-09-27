5b1995ec3c9706d74c1af56259ff5a007d75c8d6
Marcin Kulik <m@ku1ik.com>
2013-09-27 12:20:16 +0200

    Make conditionals flow more readable in exception notification initializer

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,5 @@
 airbrake_api_key = Gitorious::Configuration.get("airbrake_api_key")
+exception_recipients = Gitorious::Configuration.get("exception_recipients")
 
 if airbrake_api_key
   require 'airbrake'
@@ -13,19 +14,17 @@ if airbrake_api_key
   Gitorious::Application.config.middleware.insert_after(
     "ActionDispatch::DebugExceptions", "Airbrake::Rails::Middleware"
   )
-else
-  exception_recipients = Gitorious::Configuration.get("exception_recipients")
+elsif exception_recipients.present?
+  require "exception_notifier"
 
-  if Rails.env.production? && exception_recipients.blank?
+  Gitorious::Application.config.middleware.use(ExceptionNotifier, {
+    :email_prefix => "[Gitorious] ",
+    :sender_address => Gitorious.email_sender,
+    :exception_recipients => exception_recipients
+  })
+else
+  if Rails.env.production?
     $stderr.puts "WARNING! No value set for exception_recipients in gitorious.yml."
     $stderr.puts "Will not be able to send email regarding unhandled exceptions"
-  else
-    require "exception_notifier"
-
-    Gitorious::Application.config.middleware.use(ExceptionNotifier, {
-      :email_prefix => "[Gitorious] ",
-      :sender_address => Gitorious.email_sender,
-      :exception_recipients => exception_recipients
-    })
   end
 end
