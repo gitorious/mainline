052cf1124e7a713ac95f537295f752547b957cd4
Piotr Solnica <piotr.solnica@gmail.com>
2013-10-18 09:22:05 +0200

    Change meta-body in the event to be a span

--- app/helpers/event_rendering_helper.rb
+++ app/helpers/event_rendering_helper.rb
@@ -259,10 +259,14 @@ module EventRenderingHelper
     project = source_repository.project
     target_repository = event.target.target_repository
 
+    meta_body = content_tag(:span, :class => 'gts-event-meta-body') {
+      "&#x2192; #{sanitize(event.data.to_s)}".html_safe
+    }
+
     action = action_for_event(:event_updated_merge_request) do
       link_to(h(target_repository.url_path) + " " + h("##{event.target.to_param}"),
         project_repository_merge_request_path(project, target_repository, event.target)) +
-        ("<div class=\"meta_body\">&#x2192; " + sanitize(event.data.to_s) + "</div>").html_safe
+        meta_body.html_safe
     end
     body = truncate(h(event.body), :length => 100)
     category = "merge_request"
