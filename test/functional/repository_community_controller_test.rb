cd2e0c72fdcb2a1401271c75e14042d514ec897d
Adam Pohorecki <adam@pohorecki.pl>
2013-10-18 12:22:50 +0200

    Fix repository community tab. Closes #307

---
+++ test/functional/repository_community_controller_test.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2013 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
+class RepositoryCommunityControllerTest < ActionController::TestCase
+  context "#index" do
+    include SampleRepoHelpers
+
+    should "render clones of the repository" do
+      repository = repositories(:johans)
+
+      get :index, :project_id => repository.project.to_param, :repository_id => repository.to_param
+
+      assert response.success?
+    end
+  end
+end
