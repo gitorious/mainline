d64dd810cd829c09e7a2339b22fa70ac3fcfbba8
Christian Johansen <christian@gitorious.com>
2013-10-21 10:49:03 +0200

    Remove outdated and outcommented tests

--- test/unit/helpers/repositories_helper_test.rb
+++ test/unit/helpers/repositories_helper_test.rb
@@ -42,50 +42,7 @@ class RepositoriesHelperTest < ActionView::TestCase
     letter * 40
   end
 
-  # should "has a commit_path shortcut" do
-  #   assert_equal project_repository_commit_path(@project, @repository, "master"), commit_path
-  #   assert_equal project_repository_commit_path(@project, @repository, "foo"), commit_path("foo")
-  # end
-
-  # Commented out because rspec seems to fail at understanding helper_method, boo!
-  # it "has a log_path shortcut" do
-  #   assert_equal project_repository_commits_path(@project, @repository, ["master"]), #   helper.log_path("master")
-  # end
-  #
-  # it "has a log_path shortcut that takes args" do
-  #   assert_equal project_repository_commits_path(@project, , #   helper.log_path("master", :page => 2)
-  #     @repository, ["master"], {:page => 2})
-  # end
-  #
-  # it "has a tree_path shortcut" do
-  #  assert_equal project_repository_tree_path(@project, @repository, "master"), #   tree_path
-  # end
-  #
-  # it "has a tree_path shortcut that takes an sha1" do
-  #   assert_equal project_repository_tree_path(@project, @repository, "foo"), #   tree_path("foo")
-  # end
-  #
-  # it "has a tree_path shortcut that takes an sha1 and a path glob" do
-  #   assert_equal project_repository_tree_path(@project, , #   tree_path("foo", %w[a b c])
-  #     @repository, "foo", ["a", "b", "c"])
-  # end
-  #
-  # it "has a tree_path shortcut can takes a path string and turns it into a glob" do
-  #   assert_equal project_repository_tree_path(@project, , #   tree_path("foo", "a/b/c")
-  #     @repository, "foo", ["a", "b", "c"])
-  # end
-  #
-  # it "has a blob_path shortcut" do
-  #    assert_equal project_repository_blob_path(@project, , #   blob_path(generic_sha, ["a","b"])
-  #     @repository, generic_sha, ["a","b"])
-  # end
-  #
-  # it "has a raw_blob_path shortcut" do
-  #    assert_equal project_repository_raw_blob_path(, #   raw_blob_path(generic_sha, ["a","b"])
-  #     @project, @repository, generic_sha, ["a","b"])
-  # end
-
-  should "knows if a branch is namespaced" do
+  should "know if a branch is namespaced" do
     assert !namespaced_branch?("foo")
     assert namespaced_branch?("foo/bar")
     assert namespaced_branch?("foo/bar/baz")
