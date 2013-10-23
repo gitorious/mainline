5549f31ed89df3b2460776981ea86185ac329d14
Piotr Solnica <piotr.solnica@gmail.com>
2013-10-23 18:33:34 +0200

    Workaround issue with legacy push events having no commit events attached

--- app/presenters/event_presenter/push_event.rb
+++ app/presenters/event_presenter/push_event.rb
@@ -4,12 +4,12 @@ class EventPresenter
     COMMIT_LIMIT = 3
 
     def action
-      if single_commit?
-        commit = events.first
+      if single_commit? || commit_count == 0
+        commit = first_sha || data
 
         commit_link = link_to(
-          commit.data[0,8],
-          view.project_repository_commit_path(project, repository, commit.data)
+          commit[0,8],
+          view.project_repository_commit_path(project, repository, commit)
         )
       else
         commit_link = link_to("#{commit_count} commits", diff_url)
@@ -18,8 +18,16 @@ class EventPresenter
       action_for_event(:event_pushed_n, :commit_link => commit_link) { "to #{repository_link}" }
     end
 
+    def body
+      if commit_count > 0
+        super
+      else
+        ''
+      end
+    end
+
     def repository_link
-      link_to("#{repo_title(repository, project)}:#{data}", [project, repository])
+      link_to("#{repo_title(repository, project)}", [project, repository])
     end
 
     def commit_count
@@ -32,10 +40,14 @@ class EventPresenter
 
     private
 
+    def initialize_commits
+      super if events.size > 0
+    end
+
     def initialize_event_data
       @event_data = {
-        :start_sha    => events.first.data,
-        :end_sha      => events.last.data,
+        :start_sha    => events.first && events.first.data,
+        :end_sha      => events.last && events.last.data,
         :commit_count => events.size,
         :branch       => data
       }
