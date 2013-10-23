b7ad9e0a50f864fb7bcc4e5b6bb2ef8263da409a
Piotr Solnica <piotr.solnica@gmail.com>
2013-10-23 21:36:07 +0200

    Workaround case where update repo event points to deleted repo

--- app/presenters/event_presenter/update_repository_event.rb
+++ app/presenters/event_presenter/update_repository_event.rb
@@ -4,15 +4,23 @@ class EventPresenter
 
     def action
       action_for_event(:event_updated_repository) {
-        link_to(
-          h(target.url_path),
-          view.project_repository_path(target.project, event)
-        )
+        if repository
+          link_to(
+            h(repository.url_path),
+            view.project_repository_path(project, repository)
+          )
+        else
+          'repository was deleted'
+        end
       }
     end
 
     def category
       'repository'
+    end
+
+    def repository
+      target
     end
 
   end
