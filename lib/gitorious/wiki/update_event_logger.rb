d6077082af44e17a33091e8085654d08b6474bea
Piotr Solnica <piotr.solnica@gmail.com>
2013-10-25 16:57:56 +0200

    Add missing require

--- lib/gitorious/wiki/update_event_logger.rb
+++ lib/gitorious/wiki/update_event_logger.rb
@@ -15,6 +15,9 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
+
+require 'gitorious/wiki/commit_parser'
+
 module Gitorious
   module Wiki
     class UpdateEventLogger
