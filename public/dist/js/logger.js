2a85c28d35c6e1fad48840729fb8ad2618c64971
Christian Johansen <christian@gitorious.com>
2013-10-29 15:24:23 +0100

    Up UI build

--- public/dist/js/logger.js
+++ public/dist/js/logger.js
@@ -1,5 +1,8 @@
 /*global cull, dome, console, gts*/
 function uinitLogger(app, level) {
+    var params = window.location.href.match(/\?.*components=([^&]+)/) || [];
+    var filters = params[1] && params[1].split(",") || [];
+
     if (typeof level === "string") {
         level = uinitLogger.levels.indexOf((level || "info").toLowerCase());
     }
@@ -13,21 +16,35 @@ function uinitLogger(app, level) {
             console.log("===========================");
             console.log("Attempting to load features");
             console.log("===========================");
+
+            if (filters.length > 0) {
+                console.log("=> Only logging events for", filters.join(", "));
+            }
         });
+    }
+
+    function filtered(fn) {
+        return function (feature) {
+            if (filters.length > 0 && filters.indexOf(feature.name) < 0) {
+                // Feature is not matched by filter, abort
+                return;
+            }
+            return fn.apply(this, arguments);
+        };
     }
 
     if (level <= uinitLogger.DEBUG) {
-        app.on("loading", function (feature) {
+        app.on("loading", filtered(function (feature) {
             console.log("[Loading:", feature.name + "]");
-        });
+        }));
 
-        app.on("reloading", function (feature) {
+        app.on("reloading", filtered(function (feature) {
             console.log("[Re-loading:", feature.name + "]");
-        });
+        }));
     }
 
     if (level <= uinitLogger.INFO) {
-        app.on("pending", function (feature) {
+        app.on("pending", filtered(function (feature) {
             var name = cull.prop("name");
             var reason, pending = cull.map(name, cull.select(function (f) {
                 return !f.loaded;
@@ -51,28 +68,28 @@ function uinitLogger(app, level) {
             }
 
             console.log("[Pending:", feature.name + "]", reason);
-        });
+        }));
     }
 
     if (level <= uinitLogger.INFO) {
-        app.on("skip", function (feature) {
+        app.on("skip", filtered(function (feature) {
             console.log("[Skip:", feature.name + "]",
                         "Reload triggered, but input was unchanged");
-        });
+        }));
     }
 
     if (level <= uinitLogger.INFO) {
-        app.on("loaded", function (feature, result) {
+        app.on("loaded", filtered(function (feature, result) {
             console.log("[Load:", feature.name + "] =>", result);
-        });
+        }));
     }
 
     if (level <= uinitLogger.ERROR) {
-        app.on("error", function (feature, err) {
+        app.on("error", filtered(function (feature, err) {
             console.log("Error while loading", feature.name);
             console.log("  " + err.message);
             console.log("  " + err.stack);
-        });
+        }));
     }
 }
 
