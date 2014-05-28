0f921b4beccbf279438dd2331a5288f5d323ecb1
Marcin Kulik <m@ku1ik.com>
2014-05-28 10:07:51 +0200

    Remove YAML engine override
    Syck engine is not available in Ruby 2.0 so we'll just stick to default
    one.

--- config/initializers/override_psych.rb
+++
@@ -1,5 +0,0 @@
-# If the Psych YAML parser is installed and available, it will conflict with
-# the version of ActiveSupport shipping with Gitorious
-if defined?(Psych) && defined?(YAML)
-  YAML::ENGINE.yamler = 'syck'
-end
