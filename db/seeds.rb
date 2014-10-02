241bd901b38f1e277b5bbb8913d892edcbe54f5a
Marcin Kulik <m@ku1ik.com>
2014-10-02 13:46:07 +0200

    Create admin user when seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,19 @@ unless Role.count > 0
   Role.create!(:name => Role::ADMIN, :kind => Role::KIND_ADMIN)
   Role.create!(:name => Role::MEMBER, :kind => Role::KIND_MEMBER)
 end
+
+unless User.count > 0
+  hostname = ENV["GITORIOUS_HOSTNAME"] || `hostname`.strip
+
+  user = User.create!(
+    login: "admin",
+    password: "g1torious",
+    password_confirmation: "g1torious",
+    email: "admin@#{hostname}",
+    is_admin: true,
+    terms_of_use: true,
+    activated_at: Time.now,
+    activation_code: nil
+  )
+  user.accept_terms!
+end
