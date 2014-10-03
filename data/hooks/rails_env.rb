85b14741b11ebd41af5de9d14c6d189d715eaa2e
Marcin Kulik <m@ku1ik.com>
2014-10-03 12:25:06 +0200

    Move hooks to https://gitorious.org/gitorious/gitorious-proto

--- data/hooks/rails_env.rb
+++
@@ -1,9 +0,0 @@
-
-current_dir = File.expand_path(File.dirname(__FILE__))
-app_root = File.join(current_dir, "../../")
-
-print "=> Syncing Gitorious... "
-$stdout.flush
-ENV["RAILS_ENV"] ||= "production"
-require File.join(app_root,"/config/environment")
-
