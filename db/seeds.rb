af20a45efe0da78d4f64b136fe0b6a7dcda8756d
Marcin Kulik <m@ku1ik.com>
2014-12-01 13:59:38 +0000

    Make admin seed idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,17 @@
 Role.where(kind: Role::KIND_ADMIN).first_or_create!(name: Role::ADMIN)
 Role.where(kind: Role::KIND_MEMBER).first_or_create!(name: Role::MEMBER)
 
-hostname = ENV["GITORIOUS_HOSTNAME"] || `hostname`.strip
-admin = User.where(login: "admin").first_or_create!(
-  login: "admin",
-  password: "g1torious",
-  password_confirmation: "g1torious",
-  email: "admin@#{hostname}",
-  is_admin: true,
-  terms_of_use: true,
-  activated_at: Time.now,
-  activation_code: nil
-)
-admin.accept_terms!
+unless User.where(login: "admin").exists?
+  hostname = ENV["GITORIOUS_HOST"] || `hostname`.strip
+  admin = User.where(login: "admin").create!(
+    login: "admin",
+    password: "g1torious",
+    password_confirmation: "g1torious",
+    email: "admin@#{hostname}",
+    is_admin: true,
+    terms_of_use: true,
+    activated_at: Time.now,
+    activation_code: nil
+  )
+  admin.accept_terms!
+end
